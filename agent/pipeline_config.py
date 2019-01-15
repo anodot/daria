aa4ac27ee93f3d80dbcb35a6bf8dd40a5df82ad6
dariakharlan <daria@anodot.com>
2019-01-15 17:36:33 +0200

    config handler

---
+++ agent/pipeline_config.py
@@ -0,0 +1,83 @@
+import json
+import os
+
+from logger import get_logger
+
+logger = get_logger(__name__)
+
+
+class PipelineConfigHandler:
+    PIPELINES_BASE_CONFIGS_PATH = 'pipelines/{source_name}.json'
+
+    def __init__(self, client_config):
+        self.client_config = client_config
+        if not self.validate():
+            logger.error('Validation errors')
+            raise PipelineConfigHandlerException('Validation errors')
+
+        base_path = self.PIPELINES_BASE_CONFIGS_PATH.format(**{'source_name': client_config['source_name']})
+        with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), base_path), 'r') as f:
+            data = json.load(f)
+            self.config = data['pipelineConfig']
+            self.rules = data['pipelineRules']
+
+    def validate(self):
+        return True
+
+    def update_source_configs(self):
+        for conf in self.config['stages'][0]['configuration']:
+            if conf['name'] in self.client_config['source_config']:
+                conf['value'] = self.client_config['source_config'][conf['name']]
+
+    def update_measurement_name(self, stage):
+        for conf in stage['configuration']:
+            if conf['name'] == 'expressionProcessorConfigs':
+                conf['value'][1]['expression'] = self.client_config['measurement_name']
+
+    def rename_fields_for_anodot_protocol(self, stage):
+        for conf in stage['configuration']:
+            if conf['name'] != 'renameMapping':
+                continue
+
+            rename_mapping = [
+                {'fromFieldExpression': '/' + self.client_config['value_field_name'],
+                 'toFieldExpression': '/value'}
+            ]
+
+            if self.client_config['timestamp_field_name'] != 'timestamp':
+                rename_mapping.append({'fromFieldExpression': '/' + self.client_config['timestamp_field_name'],
+                                       'toFieldExpression': '/timestamp'})
+
+            for dim in self.client_config['dimensions']:
+                rename_mapping.append({'fromFieldExpression': '/' + dim, 'toFieldExpression': '/properties/' + dim})
+            conf['value'] = rename_mapping
+
+    def update_http_client(self):
+        for conf in self.config['stages'][len(self.config['stages']) - 1]['configuration']:
+            if conf['name'] == 'conf.resourceUrl':
+                conf['value'] = self.client_config['destination_url']
+
+    def override_base_config(self, new_uuid, new_pipeline_title):
+        self.config['uuid'] = new_uuid
+        self.config['title'] = new_pipeline_title
+
+        self.update_source_configs()
+
+        for stage in self.config['stages']:
+            if stage['instanceName'] == 'ExpressionEvaluator_01':
+                self.update_measurement_name(stage)
+
+            if stage['instanceName'] == 'FieldRenamer_01':
+                self.rename_fields_for_anodot_protocol(stage)
+
+        self.update_http_client()
+
+        return self.config
+
+    def override_base_rules(self, new_uuid):
+        self.rules['uuid'] = new_uuid
+        return self.rules
+
+
+class PipelineConfigHandlerException(Exception):
+    pass
