7dd267a58e8f6919f444051831b5d5e6051a6115
dariakharlan <daria@anodot.com>
2019-01-29 15:54:44 +0200

    put code to src dir

--- agent/setup.py
+++ agent/setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='agent',
     version='0.1',
-    packages=find_packages(),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
     include_package_data=True,
     entry_points='''
         [console_scripts]
