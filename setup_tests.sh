15e4cc3eab51a3527606f3ca7b116e5e6f3d955c
dariakharlan <daria@anodot.com>
2019-03-04 15:19:41 +0200

    setup tests

--- setup_tests.sh
+++ setup_tests.sh
@@ -4,5 +4,3 @@ docker exec agent-kafka bash -c "kafka-console-producer.sh --broker-list localho
 docker exec agent-kafka bash -c "kafka-console-producer.sh --broker-list localhost:9092 --topic test_timestamp_ms < /home/test_json_items"
 docker exec agent-kafka bash -c "kafka-console-producer.sh --broker-list localhost:9092 --topic test_timestamp_string < /home/test_json_items"
 docker exec agent-kafka bash -c "kafka-console-producer.sh --broker-list localhost:9092 --topic test_timestamp_datetime < /home/test_json_items"
-
-docker exec anodot-agent bash -c "pytest"
