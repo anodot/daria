e91662496ea72fdc19c6ae8376ec01f75c276341
dariakharlan <daria@anodot.com>
2019-07-04 18:07:00 +0300

    upgrade

--- agent/src/agent/cli.py
+++ agent/src/agent/cli.py
@@ -1,7 +1,11 @@
 import click
+import time
+
+from .pipeline import Pipeline
 from .pipeline.cli import pipeline
 from .source.cli import source
 from .destination.cli import destination
+from agent.streamsets_api_client import api_client, StreamSetsApiClientException
 
 
 @click.group()
@@ -11,7 +15,24 @@ def agent():
 
 @click.command()
 def upgrade():
-    pass
+
+    running_pipelines = []
+    for p in api_client.get_pipelines():
+        try:
+            api_client.stop_pipeline(p['pipelineId'])
+        except StreamSetsApiClientException:
+            continue
+        running_pipelines.append(p['pipelineId'])
+
+    time.sleep(3)
+    for p in api_client.get_pipelines():
+        pipeline_obj = Pipeline(p['pipelineId'])
+        pipeline_obj.load()
+        pipeline_obj.update()
+
+        if p['pipelineId'] in running_pipelines:
+            api_client.start_pipeline(pipeline_obj.id)
+        click.secho('Destination configured', fg='green')
 
 
 agent.add_command(source)
