a4e506147a2f5006a2d720663142d3cbad09652e
dariakharlan <daria@anodot.com>
2019-08-27 15:10:02 +0300

    prompt kafka

--- agent/src/agent/source/kafka.py
+++ agent/src/agent/source/kafka.py
@@ -1,56 +1,75 @@
 import click
 
 from .abstract_source import Source
+from agent.tools import infinite_retry
+from kafka import KafkaConsumer
 
 
 class KafkaSource(Source):
+    CONFIG_BROKER_LIST = 'kafkaConfigBean.metadataBrokerList'
+    CONFIG_CONSUMER_GROUP = 'kafkaConfigBean.consumerGroup'
+    CONFIG_TOPIC = 'kafkaConfigBean.topic'
+    CONFIG_OFFSET_TYPE = 'kafkaConfigBean.kafkaAutoOffsetReset'
+    CONFIG_OFFSET_TIMESTAMP = 'kafkaConfigBean.timestampToSearchOffsets'
+    CONFIG_BATCH_SIZE = 'kafkaConfigBean.maxBatchSize'
+    CONFIG_BATCH_WAIT_TIME = 'kafkaConfigBean.maxWaitTime'
+    CONFIG_CONSUMER_PARAMS = 'kafkaConfigBean.kafkaConsumerConfigs'
+
+    OFFSET_EARLIEST = 'EARLIEST'
+    OFFSET_LATEST = 'LATEST'
+    OFFSET_TIMESTAMP = 'TIMESTAMP'
+
+    @infinite_retry
+    def prompt_connection(self, default_config, advanced=False):
+        self.config[self.CONFIG_BROKER_LIST] = click.prompt('Kafka broker connection string',
+                                                            type=click.STRING,
+                                                            default=default_config.get(self.CONFIG_BROKER_LIST))
+        if advanced:
+            self.prompt_consumer_params(default_config)
+
+    @infinite_retry
+    def prompt_consumer_params(self, default_config):
+        default_kafka_config = default_config.get(self.CONFIG_CONSUMER_PARAMS, '')
+        if default_kafka_config:
+            default_kafka_config = ' '.join([i['key'] + ':' + i['value'] for i in default_kafka_config])
+        kafka_config = click.prompt('Kafka Configuration', type=click.STRING, default=default_kafka_config)
+        self.config[self.CONFIG_CONSUMER_PARAMS] = []
+        for i in kafka_config.split(','):
+            pair = i.split(':')
+            if len(pair) != 2:
+                raise click.UsageError('Wrong format')
+
+            self.config[self.CONFIG_CONSUMER_PARAMS].append({'key': pair[0], 'value': pair[1]})
+
+    @infinite_retry
+    def prompt_topic(self, default_config):
+        self.config[self.CONFIG_TOPIC] = click.prompt('Topic', type=click.STRING,
+                                                      default=default_config.get(self.CONFIG_TOPIC))
 
     def prompt(self, default_config, advanced=False):
-        config = dict()
-        config['kafkaConfigBean.metadataBrokerList'] = click.prompt('Kafka broker connection string',
-                                                                    type=click.STRING,
-                                                                    default=default_config.get(
-                                                                        'kafkaConfigBean.metadataBrokerList'))
-        config['kafkaConfigBean.consumerGroup'] = click.prompt('Consumer group',
-                                                               type=click.STRING,
-                                                               default=default_config.get(
-                                                                   'kafkaConfigBean.consumerGroup',
-                                                                   'anodotAgent'))
-        config['kafkaConfigBean.topic'] = click.prompt('Topic', type=click.STRING,
-                                                       default=default_config.get('kafkaConfigBean.topic'))
-        config['kafkaConfigBean.kafkaAutoOffsetReset'] = click.prompt('Initial offset',
-                                                                      type=click.Choice(
-                                                                          ['EARLIEST', 'LATEST', 'TIMESTAMP']),
-                                                                      default=default_config.get(
-                                                                          'kafkaConfigBean.kafkaAutoOffsetReset',
-                                                                          'EARLIEST'))
-        if config['kafkaConfigBean.kafkaAutoOffsetReset'] == 'TIMESTAMP':
-            config['kafkaConfigBean.timestampToSearchOffsets'] = click.prompt(
+        self.config = dict()
+        self.prompt_connection(default_config, advanced)
+
+        self.config[self.CONFIG_CONSUMER_GROUP] = click.prompt('Consumer group', type=click.STRING,
+                                                               default=default_config.get(self.CONFIG_CONSUMER_GROUP,
+                                                                                          'anodotAgent'))
+        self.config[self.CONFIG_OFFSET_TYPE] = click.prompt('Initial offset',
+                                                            type=click.Choice([self.OFFSET_EARLIEST, self.OFFSET_LATEST,
+                                                                               self.OFFSET_TIMESTAMP]),
+                                                            default=default_config.get(self.CONFIG_OFFSET_TYPE,
+                                                                                       self.OFFSET_EARLIEST))
+        if self.config[self.CONFIG_OFFSET_TYPE] == self.OFFSET_TIMESTAMP:
+            self.config[self.CONFIG_OFFSET_TIMESTAMP] = click.prompt(
                 'Offset timestamp (unix timestamp in milliseconds)',
                 type=click.STRING,
-                default=default_config.get('kafkaConfigBean.timestampToSearchOffsets'))
+                default=default_config.get(self.CONFIG_OFFSET_TIMESTAMP))
 
         if advanced:
-            config['kafkaConfigBean.maxBatchSize'] = click.prompt('Max Batch Size (records)',
-                                                                  type=click.INT,
-                                                                  default=default_config.get(
-                                                                      'kafkaConfigBean.maxBatchSize',
-                                                                      1000))
-            config['kafkaConfigBean.maxWaitTime'] = click.prompt('Batch Wait Time (ms)',
-                                                                 type=click.INT,
-                                                                 default=default_config.get(
-                                                                     'kafkaConfigBean.maxWaitTime',
-                                                                     1000))
+            self.config[self.CONFIG_BATCH_SIZE] = click.prompt('Max Batch Size (records)', type=click.INT,
+                                                               default=default_config.get(self.CONFIG_BATCH_SIZE, 1000))
+            self.config[self.CONFIG_BATCH_WAIT_TIME] = click.prompt('Batch Wait Time (ms)', type=click.INT,
+                                                                    default=default_config.get(
+                                                                        self.CONFIG_BATCH_WAIT_TIME,
+                                                                        1000))
 
-            default_kafka_config = default_config.get('kafkaConfigBean.kafkaConsumerConfigs', '')
-            if default_kafka_config:
-                default_kafka_config = ' '.join([i['key'] + ':' + i['value'] for i in default_kafka_config])
-            kafka_config = click.prompt('Kafka Configuration', type=click.STRING, default=default_kafka_config)
-            config['kafkaConfigBean.kafkaConsumerConfigs'] = []
-            for i in kafka_config.split(','):
-                pair = i.split(':')
-                if len(pair) != 2:
-                    raise click.UsageError('Wrong format')
-
-                config['kafkaConfigBean.kafkaConsumerConfigs'].append({'key': pair[0], 'value': pair[1]})
-        return config
+        return self.config
