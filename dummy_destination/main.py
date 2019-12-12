fb5f1faa17e9cc296a7f715b6945d1fd84b74f13
Дар'я Харлан <dadaka.x@gmail.com>
2019-12-12 15:19:00 +0200

    dumm_destination output to different files

--- dummy_destination/main.py
+++ dummy_destination/main.py
@@ -3,7 +3,7 @@ import json
 import time
 from flask import Flask, request
 
-OUTPUT_FILE = os.environ.get('OUTPUT_FILE', 'output.log')
+OUTPUT_DIR = os.environ.get('OUTPUT_DIR', 'log')
 
 
 app = Flask(__name__)
@@ -12,10 +12,11 @@ app.secret_key = b"\xf9\x19\x8d\xd2\xb7N\x84\xae\x16\x0f'`U\x88x&\nF\xa2\xe9\xa1
 
 @app.route('/api/v1/metrics', methods=['POST', 'GET'])
 def to_file():
-
-    with open(OUTPUT_FILE, 'a+') as f:
-        json.dump(request.json, f)
-        f.write('\n')
+    data = request.json
+    if len(data) > 0:
+        with open(os.path.join(OUTPUT_DIR, data[0]['tags']['pipeline_id'][0] + '.json'), 'a+') as f:
+            json.dump(request.json, f)
+            f.write('\n')
     return 'ok'
 
 
