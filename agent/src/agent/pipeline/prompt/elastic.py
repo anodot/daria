cc1d6566d0604f93ac41ab4667f3d1f29884e78c
dariakharlan <daria@anodot.com>
2020-02-03 13:15:32 +0200

    no transformations

--- agent/src/agent/pipeline/prompt/elastic.py
+++ agent/src/agent/pipeline/prompt/elastic.py
@@ -4,3 +4,12 @@ from .json import PromptConfigJson
 
 class PromptConfigElastic(PromptConfigJson):
     timestamp_types = ['datetime', 'string', 'unix', 'unix_ms']
+
+    def set_config(self):
+        self.data_preview()
+        self.set_values()
+        self.set_measurement_names()
+        self.set_timestamp()
+        self.set_dimensions()
+        self.set_static_properties()
+        self.set_tags()
