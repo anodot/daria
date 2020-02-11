957c7aeb2a6719ebf3d727d6059d39464097872c
Daria Kharlan <dariakharlan@users.noreply.github.com>
2020-02-11 18:02:59 +0200

    Stop deprecated pipeline (#27)
    * stop deprecated pipeline
    
    * test

--- agent/src/agent/pipeline/pipeline.py
+++ agent/src/agent/pipeline/pipeline.py
@@ -1,12 +1,10 @@
 import click
 import json
 import os
-import time
 
 from .. import source
 from agent.constants import DATA_DIR
 from agent.destination import HttpDestination
-from agent.streamsets_api_client import api_client
 
 
 class Pipeline:
@@ -51,66 +49,10 @@ class Pipeline:
         with open(self.file_path, 'w') as f:
             json.dump(self.to_dict(), f)
 
-    def check_status(self, status):
-        response = api_client.get_pipeline_status(self.id)
-        return response['status'] == status
-
-    def wait_for_status(self, status, tries=5, initial_delay=3):
-        for i in range(1, tries + 1):
-            response = api_client.get_pipeline_status(self.id)
-            if response['status'] == status:
-                return True
-            delay = initial_delay ** i
-            if i == tries:
-                raise PipelineFreezeException(f"Pipeline {self.id} is still {response['status']} after {tries} tries")
-            print(f"Pipeline {self.id} is {response['status']}. Check again after {delay} seconds...")
-            time.sleep(delay)
-
-    def wait_for_sending_data(self, tries=5, initial_delay=2):
-        for i in range(1, tries + 1):
-            response = api_client.get_pipeline_metrics(self.id)
-            stats = {
-                'in': response['counters']['pipeline.batchInputRecords.counter']['count'],
-                'out': response['counters']['pipeline.batchOutputRecords.counter']['count'],
-                'errors': response['counters']['pipeline.batchErrorRecords.counter']['count'],
-            }
-            if stats['out'] > 0 and stats['errors'] == 0:
-                return True
-            if stats['errors'] > 0:
-                raise PipelineException(f"Pipeline {self.id} is has {stats['errors']} errors")
-            delay = initial_delay ** i
-            if i == tries:
-                raise PipelineException(f"Pipeline {self.id} did not send any data. Received number of records - {stats['in']}")
-            print(f'Waiting for pipeline {self.id} to send data. Check again after {delay} seconds...')
-            time.sleep(delay)
-
-    def stop(self):
-        api_client.stop_pipeline(self.id)
-        try:
-            self.wait_for_status(self.STATUS_STOPPED)
-        except PipelineFreezeException:
-            print("Force stopping the pipeline")
-            self.force_stop()
-
-    def force_stop(self):
-        if not self.check_status(self.STATUS_STOPPING):
-            raise PipelineException("Can't force stop a pipeline not in the STOPPING state")
-
-        api_client.force_stop_pipeline(self.id)
-        self.wait_for_status(self.STATUS_STOPPED)
-
-    def start(self):
-        api_client.start_pipeline(self.id)
-        self.wait_for_status(self.STATUS_RUNNING)
-
 
 class PipelineException(click.ClickException):
     pass
 
 
 class PipelineNotExistsException(PipelineException):
-    pass
-
-
-class PipelineFreezeException(PipelineException):
     pass
--- agent/src/agent/pipeline/pipeline_manager.py
+++ agent/src/agent/pipeline/pipeline_manager.py
@@ -1,11 +1,12 @@
 import click
 import os
 import shutil
+import time
 
 from .pipeline import Pipeline, PipelineException
 from . import prompt, config_handlers, load_client_data
 from .. import source
-from agent.constants import ERRORS_DIR
+from agent.constants import ERRORS_DIR, ENV_PROD
 from agent.streamsets_api_client import api_client, StreamSetsApiClientException
 from agent.tools import print_json, sdc_record_map_to_dict, if_validation_enabled
 
@@ -40,6 +41,75 @@ handlers = {
 }
 
 
+def check_status(pipeline_id: str, status: str):
+    response = api_client.get_pipeline_status(pipeline_id)
+    return response['status'] == status
+
+
+def wait_for_status(pipeline_id: str, status: str, tries: int = 5, initial_delay: int = 3):
+    for i in range(1, tries + 1):
+        response = api_client.get_pipeline_status(pipeline_id)
+        if response['status'] == status:
+            return True
+        delay = initial_delay ** i
+        if i == tries:
+            raise PipelineFreezeException(f"Pipeline {pipeline_id} is still {response['status']} after {tries} tries")
+        print(f"Pipeline {pipeline_id} is {response['status']}. Check again after {delay} seconds...")
+        time.sleep(delay)
+
+
+def wait_for_sending_data(pipeline_id: str, tries: int = 5, initial_delay: int = 2):
+    for i in range(1, tries + 1):
+        response = api_client.get_pipeline_metrics(pipeline_id)
+        stats = {
+            'in': response['counters']['pipeline.batchInputRecords.counter']['count'],
+            'out': response['counters']['pipeline.batchOutputRecords.counter']['count'],
+            'errors': response['counters']['pipeline.batchErrorRecords.counter']['count'],
+        }
+        if stats['out'] > 0 and stats['errors'] == 0:
+            return True
+        if stats['errors'] > 0:
+            raise PipelineException(f"Pipeline {pipeline_id} is has {stats['errors']} errors")
+        delay = initial_delay ** i
+        if i == tries:
+            raise PipelineException(f"Pipeline {pipeline_id} did not send any data. Received number of records - {stats['in']}")
+        print(f'Waiting for pipeline {pipeline_id} to send data. Check again after {delay} seconds...')
+        time.sleep(delay)
+
+
+def force_stop_pipeline(pipeline_id: str):
+    if not check_status(pipeline_id, Pipeline.STATUS_STOPPING):
+        raise PipelineException("Can't force stop a pipeline not in the STOPPING state")
+
+    api_client.force_stop_pipeline(pipeline_id)
+    wait_for_status(pipeline_id, Pipeline.STATUS_STOPPED)
+
+
+def stop_pipeline(pipeline_id: str):
+    print("Stopping the pipeline")
+    api_client.stop_pipeline(pipeline_id)
+    try:
+        wait_for_status(pipeline_id, Pipeline.STATUS_STOPPED)
+    except PipelineFreezeException:
+        print("Force stopping the pipeline")
+        force_stop_pipeline(pipeline_id)
+
+
+def delete_pipeline(pipeline_id: str):
+    try:
+        if check_status(pipeline_id, Pipeline.STATUS_RUNNING):
+            stop_pipeline(pipeline_id)
+
+        api_client.delete_pipeline(pipeline_id)
+        if Pipeline.exists(pipeline_id):
+            os.remove(Pipeline.get_file_path(pipeline_id))
+        errors_dir = os.path.join(ERRORS_DIR, pipeline_id)
+        if os.path.isdir(errors_dir):
+            shutil.rmtree(errors_dir)
+    except StreamSetsApiClientException as e:
+        raise PipelineException(str(e))
+
+
 class PipelineManager:
     def __init__(self, pipeline: Pipeline):
         self.pipeline = pipeline
@@ -69,8 +139,8 @@ class PipelineManager:
     def update(self):
         start_pipeline = False
         try:
-            if self.pipeline.check_status(self.pipeline.STATUS_RUNNING):
-                self.pipeline.stop()
+            if check_status(self.pipeline.id, Pipeline.STATUS_RUNNING):
+                self.stop()
                 start_pipeline = True
 
             pipeline_obj = api_client.get_pipeline(self.pipeline.id)
@@ -82,7 +152,7 @@ class PipelineManager:
             raise PipelineException(str(e))
         finally:
             if start_pipeline:
-                self.pipeline.start()
+                self.start()
 
         self.pipeline.save()
 
@@ -93,20 +163,23 @@ class PipelineManager:
         except (config_handlers.ConfigHandlerException, StreamSetsApiClientException) as e:
             raise PipelineException(str(e))
 
-    @classmethod
-    def delete_pipeline(cls, pipeline_id: str):
-        try:
-            api_client.delete_pipeline(pipeline_id)
-            if Pipeline.exists(pipeline_id):
-                os.remove(Pipeline.get_file_path(pipeline_id))
-            errors_dir = os.path.join(ERRORS_DIR, pipeline_id)
-            if os.path.isdir(errors_dir):
-                shutil.rmtree(errors_dir)
-        except StreamSetsApiClientException as e:
-            raise PipelineException(str(e))
-
     def delete(self):
-        self.delete_pipeline(self.pipeline.id)
+        delete_pipeline(self.pipeline.id)
+
+    def enable_destination_logs(self, enable):
+        self.pipeline.destination.enable_logs(enable)
+        self.update()
+
+    def stop(self):
+        stop_pipeline(self.pipeline.id)
+
+    def start(self):
+        api_client.start_pipeline(self.pipeline.id)
+        wait_for_status(self.pipeline.id, Pipeline.STATUS_RUNNING)
+        click.secho(f'{self.pipeline.id} pipeline is running')
+        if ENV_PROD:
+            wait_for_sending_data(self.pipeline.id)
+            click.secho(f'{self.pipeline.id} pipeline is sending data')
 
     @if_validation_enabled
     def show_preview(self):
@@ -119,13 +192,13 @@ class PipelineManager:
 
         for output in preview_data['batchesOutput'][0]:
             if 'destination_OutputLane' in output['output']:
-                data = output['output']['destination_OutputLane'][:self.pipeline.source.MAX_SAMPLE_RECORDS]
+                data = output['output']['destination_OutputLane'][:source.Source.MAX_SAMPLE_RECORDS]
                 if data:
                     print_json([sdc_record_map_to_dict(record['value']) for record in data])
                 else:
                     print('Could not fetch any data matching the provided config')
                 break
 
-    def enable_destination_logs(self, enable):
-        self.pipeline.destination.enable_logs(enable)
-        self.update()
+
+class PipelineFreezeException(PipelineException):
+    pass
