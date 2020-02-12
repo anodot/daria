4ad259f5b529546d11ce2eab73186e2881ced9d8
dariakharlan <daria@anodot.com>
2020-02-12 19:04:42 +0200

    send monitoring metric to /agents endpoint

--- dummy_destination/main.py
+++ dummy_destination/main.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.secret_key = b"\xf9\x19\x8d\xd2\xb7N\x84\xae\x16\x0f'`U\x88x&\nF\xa2\xe9\xa1\xd7\x8b\t"
 
 
-@app.route('/api/v1/metrics', methods=['POST', 'GET'])
+@app.route('/api/v1/metrics', methods=['POST'])
 def to_file():
     data = request.json
     if data and len(data) > 0:
@@ -18,6 +18,11 @@ def to_file():
         with open(os.path.join(OUTPUT_DIR, file_name + '.json'), 'a+') as f:
             json.dump(request.json, f)
             f.write('\n')
+    return json.dumps({'errors': []})
+
+
+@app.route('/api/v1/agents', methods=['POST'])
+def monitoring_api_mock():
     return json.dumps({'errors': []})
 
 
