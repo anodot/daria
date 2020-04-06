449f3d95911a268ef829f6eceda00eeffecdd08d
Vova Nekhai <vova.nehai@anodot.com>
2020-04-06 12:26:53 +0300

    fix kafka lib version

---
+++ streamsets/configure_image.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+function configure_sql_drivers() {
+  local  MSQL_DRIVER_DIR=/opt/sdc-extras/streamsets-datacollector-jdbc-lib/lib/
+  local  MYSQL_CONNECTOR_VERSION=mysql-connector-java-8.0.18
+
+  sudo mkdir -p ${MSQL_DRIVER_DIR}
+  sudo chown -R sdc:sdc /opt/sdc-extras
+  wget https://cdn.mysql.com//Downloads/Connector-J/${MYSQL_CONNECTOR_VERSION}.tar.gz -O ${MSQL_DRIVER_DIR}/mysql_j.tar.gz
+
+  tar -xzf ${MSQL_DRIVER_DIR}/mysql_j.tar.gz -C ${MSQL_DRIVER_DIR}
+  mv ${MSQL_DRIVER_DIR}/${MYSQL_CONNECTOR_VERSION}/${MYSQL_CONNECTOR_VERSION}.jar ${MSQL_DRIVER_DIR}/${MYSQL_CONNECTOR_VERSION}.jar
+  rm -rf ${MSQL_DRIVER_DIR}/mysql_j.tar.gz
+  rm -rf ${MSQL_DRIVER_DIR}/${MYSQL_CONNECTOR_VERSION}
+}
+
+function install_stage_libs() {
+  "${SDC_DIST}/bin/streamsets" stagelibs -install="streamsets-datacollector-mongodb_3-lib,streamsets-datacollector-apache-kafka_2_0-lib,streamsets-datacollector-jdbc-lib,streamsets-datacollector-elasticsearch_5-lib"
+}
+
+# temporary function to fix https://issues.streamsets.com/browse/SDC-14063
+function apply_kafka_consumer_fix() {
+    wget https://github.com/kiamran/datacollector/releases/download/3.14-consumer-fix/release.zip -O /tmp/release.zip
+    sudo unzip /tmp/release.zip -d /tmp
+
+    sudo chown sdc:sdc /tmp/container/target/*.jar
+    sudo chown sdc:sdc /tmp/kafka_multisource-protolib/target/*.jar
+
+    sudo cp -fv /tmp/container/target/streamsets-datacollector-container-3.14.1-SNAPSHOT.jar "${SDC_DIST}"/container-lib/streamsets-datacollector-container-3.14.0.jar
+    sudo cp -fv /tmp/container/target/streamsets-datacollector-container-3.14.1-SNAPSHOT.jar "${SDC_DIST}"/streamsets-libs/streamsets-datacollector-stats-lib/lib/streamsets-datacollector-container-3.14.0.jar
+    sudo cp -fv /tmp/kafka_multisource-protolib/target/streamsets-datacollector-kafka_multisource-protolib-3.14.1-SNAPSHOT.jar "${SDC_DIST}"/streamsets-libs/streamsets-datacollector-apache-kafka_2_0-lib/lib/streamsets-datacollector-kafka_multisource-protolib-3.14.0.jar
+}
+
+############################################### execution start here #################################################
+configure_sql_drivers
+install_stage_libs
+apply_kafka_consumer_fix
