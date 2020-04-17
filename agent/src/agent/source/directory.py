bd752bffda8d5244e60ab1547978007adf5ede20
dariakharlan <daria@anodot.com>
2020-04-17 16:19:12 +0300

    directory collector tests

--- agent/src/agent/source/directory.py
+++ agent/src/agent/source/directory.py
@@ -1,31 +1,28 @@
 import click
-import socket
 
 from agent.tools import infinite_retry
 from .schemaless import SchemalessSource
 
 
 class DirectorySource(SchemalessSource):
-    CONFIG_PORTS = 'conf.ports'
-    TEST_PIPELINE_NAME = 'test_tcp_server_jksrj322'
+    CONFIG_DIR = 'conf.spoolDir'
+    CONFIG_FILE_PATTERN = 'conf.filePattern'
+    CONFIG_SPOOLING_PERIOD = 'conf.spoolingPeriod'
 
-    VALIDATION_SCHEMA_FILE_NAME = 'tcp_server.json'
+    TEST_PIPELINE_NAME = 'test_directory_ksdjfjk21'
 
-    @infinite_retry
-    def prompt_ports(self, default_config):
-        self.config[self.CONFIG_PORTS] = click.prompt('Port', type=click.STRING,
-                                                      value_proc=lambda x: x.split(','),
-                                                      default=default_config.get(self.CONFIG_PORTS))
-        for port in self.config[self.CONFIG_PORTS]:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-                sock.bind(('0.0.0.0', int(port)))
+    VALIDATION_SCHEMA_FILE_NAME = 'directory.json'
 
     def prompt(self, default_config, advanced=False):
-        self.prompt_ports(default_config)
+        self.config[self.CONFIG_DIR] = click.prompt('Directory path',
+                                                    type=click.Path(dir_okay=True, file_okay=False),
+                                                    default=default_config.get(self.CONFIG_DIR))
+        self.config[self.CONFIG_FILE_PATTERN] = click.prompt('File name pattern',
+                                                             type=click.STRING,
+                                                             default=default_config.get(self.CONFIG_FILE_PATTERN, '*'))
 
         self.prompt_data_format(default_config)
-        if advanced:
-            self.prompt_batch_size(default_config)
+        self.prompt_batch_size(default_config)
         return self.config
 
     def validate(self):
