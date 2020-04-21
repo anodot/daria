172f9f377ff0b85ed01898abfd414de434061737
dariakharlan <daria@anodot.com>
2020-04-21 12:04:38 +0300

    fix test order run

--- agent/tests/test_pipelines/test_zpipeline_base.py
+++ agent/tests/test_pipelines/test_zpipeline_base.py
@@ -56,14 +56,14 @@ class TestPipelineBase(object):
         result = cli_runner.invoke(pipeline_cli.info, [name])
         assert result.exit_code == 0
 
-    def test_reset(self, cli_runner, name):
-        result = cli_runner.invoke(pipeline_cli.reset, [name])
-        assert result.exit_code == 0
-
     def test_stop(self, cli_runner, name):
         result = cli_runner.invoke(pipeline_cli.stop, [name])
         assert result.exit_code == 0
         assert api_client.get_pipeline_status(name)['status'] in ['STOPPED']
+
+    def test_reset(self, cli_runner, name):
+        result = cli_runner.invoke(pipeline_cli.reset, [name])
+        assert result.exit_code == 0
 
     def test_output(self, name, pipeline_type, output):
         with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), f'expected_output/{output}')) as f:
