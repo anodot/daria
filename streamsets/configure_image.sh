10c7aaa435d6481dc697bbba62da9132f4a55671
Vova Nekhai <vova.nehai@anodot.com>
2020-04-28 21:08:17 +0300

    fix mssql dependency

--- streamsets/configure_image.sh
+++ streamsets/configure_image.sh
@@ -5,16 +5,12 @@ set -u
 
 function configure_sql_drivers() {
   local  MSQL_DRIVER_DIR=/opt/sdc-extras/streamsets-datacollector-jdbc-lib/lib/
-  local  MYSQL_CONNECTOR_VERSION=mysql-connector-java-8.0.18
+  local  MYSQL_CONNECTOR_VERSION=8.0.18
 
   sudo mkdir -p ${MSQL_DRIVER_DIR}
   sudo chown -R sdc:sdc /opt/sdc-extras
-  wget https://cdn.mysql.com//Downloads/Connector-J/${MYSQL_CONNECTOR_VERSION}.tar.gz -O ${MSQL_DRIVER_DIR}/mysql_j.tar.gz
-
-  tar -xzf ${MSQL_DRIVER_DIR}/mysql_j.tar.gz -C ${MSQL_DRIVER_DIR}
-  mv ${MSQL_DRIVER_DIR}/${MYSQL_CONNECTOR_VERSION}/${MYSQL_CONNECTOR_VERSION}.jar ${MSQL_DRIVER_DIR}/${MYSQL_CONNECTOR_VERSION}.jar
-  rm -rf ${MSQL_DRIVER_DIR}/mysql_j.tar.gz
-  rm -rf ${MSQL_DRIVER_DIR}/${MYSQL_CONNECTOR_VERSION}
+  wget https://repo1.maven.org/maven2/mysql/mysql-connector-java/${MYSQL_CONNECTOR_VERSION}/mysql-connector-java-${MYSQL_CONNECTOR_VERSION}.jar -O ${MSQL_DRIVER_DIR}/${MYSQL_CONNECTOR_VERSION}.jar
+  unzip -l -q ${MSQL_DRIVER_DIR}/${MYSQL_CONNECTOR_VERSION}.jar
 }
 
 function install_stage_libs() {
