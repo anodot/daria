028aa308b8c6274b8ead1b6eed593c461925fdb5
Daria Kharlan <dariakharlan@users.noreply.github.com>
2020-05-19 16:36:28 +0300

    setup agent locally (#56)
    * setup agent locally
    
    * debug.py
    
    * fix test build
    
    * fix test build
    
    Co-authored-by: dariakharlan <daria@anodot.com>

---
+++ debug.py
@@ -0,0 +1,7 @@
+from agent import cli
+from agent.pipeline import cli as pipeline_cli
+from agent.source import cli as source_cli
+
+# cli.destination()
+# source_cli.create()
+pipeline_cli.create()
---
+++ local-setup.sh
@@ -0,0 +1,10 @@
+mkdir agent-data
+export DATA_DIR="agent-data" STREAMSETS_USERNAME="admin" STREAMSETS_PASSWORD="admin" STREAMSETS_URL="http://localhost:18630" \
+    LOG_FILE_PATH="agent.log" ANODOT_API_URL="http://localhost:8080" SDC_DATA_PATH="sdc-data" ENV_PROD='false' \
+    GIT_SHA1=local-build VALIDATION_ENABLED="true"
+
+# Copy this to Pycharm debugger config
+# DATA_DIR=agent-data;STREAMSETS_USERNAME=admin;STREAMSETS_PASSWORD=admin;STREAMSETS_URL=http://localhost:18630;LOG_FILE_PATH=agent.log;ANODOT_API_URL=http://localhost:8080;SDC_DATA_PATH=sdc-data;ENV_PROD=false;GIT_SHA1=local-build;VALIDATION_ENABLED=true
+
+pip install --upgrade pip && pip install -r agent/requirements.txt
+pip install --editable agent
