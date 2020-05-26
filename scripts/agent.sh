ed9d7d945522badc99b5dd51a629b8faed1bca7e
Anton Zelenin <anton@anodot.com>
2020-05-26 13:39:18 +0300

    Added a script options to set heap and thread pool size for dc

--- scripts/agent.sh
+++ scripts/agent.sh
@@ -1,20 +1,23 @@
-num='^[0-9]+$'
-
 if [[ $1 == 'install' ]]; then
-  cp docker-compose.base docker-compose.yaml
-  sed -i '' -e "s/SDC_JAVA_OPTS_VALUE/${SDC_JAVA_OPTS} -Xmx1024m -Xms1024m -server/"
-  sed -i '' -e "s/SDC_THREAD_POOL_SIZE_VALUE/50/"
   docker-compose pull && docker-compose up -d
 elif [[ $1 == 'run' || $1 == 'rerun' ]]; then
     docker-compose up -d
 elif [[ $1 == 'attach' ]]; then
-  docker attach anodot-agent-my
+  docker attach anodot-agent
 elif [[ $1 == 'set-heap-size' ]]; then
-  if [[ $2 =~ $num ]]; then
-    echo "Please use only digits to set the heap size"
+  if ! [[ $2 =~ ^[0-9]+$ ]]; then
+    echo "Please use only integers to set the heap size"
     exit 1
   fi
-  sed -i '' -e "s/SDC_JAVA_OPTS_VALUE/${SDC_JAVA_OPTS} -Xmx$2m -Xms$2m -server/"
+  sed -i '' -E 's/SDC_JAVA_OPTS.*/SDC_JAVA_OPTS: "-Xmx'$2'm -Xms'$2'm -server"/' docker-compose.yaml
+  docker-compose up -d
+elif [[ $1 == 'set-thread-pool-size' ]]; then
+  if ! [[ $2 =~ ^[0-9]+$ ]]; then
+    echo "Please use only integers to set the thread pool size"
+    exit 1
+  fi
+  sed -i '' -E 's/SDC_CONF_RUNNER_THREAD_POOL_SIZE.*/SDC_CONF_RUNNER_THREAD_POOL_SIZE: "'$2'"/' docker-compose.yaml
+  docker-compose up -d
 else
-  echo "Wrong command supplied. Please use ./agent [install|run|rerun|attach]"
+  echo "Wrong command supplied. Please use ./agent [install|run|rerun|attach|set-heap-size|set-thread-pool-size]"
 fi
