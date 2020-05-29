6130d2f995f5fc506a7d80d6786fd21d4226cb6a
Anton Zelenin <anton@anodot.com>
2020-05-29 17:28:23 +0300

    Ask for mysql and postgres interval in pipeline, not in source

--- agent/tests/test_input/test_mysql_http.py
+++ agent/tests/test_input/test_mysql_http.py
@@ -18,18 +18,20 @@ class TestMySQL:
     }
 
     def test_source_create(self, cli_runner, name, type, conn):
-        result = cli_runner.invoke(source_cli.create, input=f"{type}\n{name}\n{conn}\n\n\n\n")
+        result = cli_runner.invoke(source_cli.create, input=f"{type}\n{name}\n{conn}\n\n\n")
         assert result.exit_code == 0
         assert os.path.isfile(os.path.join(Source.DIR, f'{name}.json'))
 
     def test_create(self, cli_runner, name, source, timestamp_type, timestamp_name):
         result = cli_runner.invoke(pipeline_cli.create,
-                                   input=f'{source}\n{name}\ntest\n\n\n1000\n\n\nclicks:gauge impressions:gauge\n{timestamp_name}\n{timestamp_type}\nadsize country\n\n\n')
+                                   input=f'{source}\n{name}\ntest\n\n\n1000\n\n\nclicks:gauge impressions:gauge\n{timestamp_name}\n{timestamp_type}\nadsize country\n\n\n\n')
+        print(result.output)
         assert result.exit_code == 0
         assert api_client.get_pipeline(name)
 
     def test_create_advanced(self, cli_runner, name, source):
         result = cli_runner.invoke(pipeline_cli.create, ['-a'],
                                    input=f'{source}\n{name}\ntest\n\n\n1000\n\ny\nclicks:gauge impressions:gauge\ntimestamp_unix\nunix\nadsize country\nkey1:val1 key2:val2\n\ncountry = \'USA\'\n\n\n')
+        print(result.output)
         assert result.exit_code == 0
         assert api_client.get_pipeline(name)
