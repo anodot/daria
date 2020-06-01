b8981fddde53c5db3ed3a226b50c6f15ddc2db77
dariakharlan <daria@anodot.com>
2020-06-01 13:19:57 +0300

    install hive dependencies

--- streamsets/configure_image.sh
+++ streamsets/configure_image.sh
@@ -4,13 +4,26 @@ set -e
 set -u
 
 function configure_sql_drivers() {
-  local  MSQL_DRIVER_DIR=/opt/sdc-extras/streamsets-datacollector-jdbc-lib/lib/
-  local  MYSQL_CONNECTOR_VERSION=8.0.18
+  local JDBC_DRIVER_DIR=/opt/sdc-extras/streamsets-datacollector-jdbc-lib/lib/
+  local MYSQL_CONNECTOR_VERSION=8.0.18
+  local IVY_VERSION=2.3.0
 
-  sudo mkdir -p ${MSQL_DRIVER_DIR}
+  sudo mkdir -p ${JDBC_DRIVER_DIR}
   sudo chown -R sdc:sdc /opt/sdc-extras
-  wget https://repo1.maven.org/maven2/mysql/mysql-connector-java/${MYSQL_CONNECTOR_VERSION}/mysql-connector-java-${MYSQL_CONNECTOR_VERSION}.jar -O ${MSQL_DRIVER_DIR}/${MYSQL_CONNECTOR_VERSION}.jar
-  unzip -l -q ${MSQL_DRIVER_DIR}/${MYSQL_CONNECTOR_VERSION}.jar
+  wget https://repo1.maven.org/maven2/mysql/mysql-connector-java/${MYSQL_CONNECTOR_VERSION}/mysql-connector-java-${MYSQL_CONNECTOR_VERSION}.jar -O ${JDBC_DRIVER_DIR}/${MYSQL_CONNECTOR_VERSION}.jar
+  unzip -l -q ${JDBC_DRIVER_DIR}/${MYSQL_CONNECTOR_VERSION}.jar
+
+  curl -L http://search.maven.org/remotecontent?filepath=org/apache/ivy/ivy/${IVY_VERSION}/ivy-${IVY_VERSION}.jar -o /tmp/ivy-${IVY_VERSION}.jar
+  echo '<ivysettings>
+    <settings defaultResolver="chain"/>
+    <resolvers>
+        <chain name="chain">
+            <ibiblio name="central" m2compatible="true"/>
+            <ibiblio name="maven-https" m2compatible="true" root="https://repo1.maven.org/maven2/"/>
+        </chain>
+    </resolvers>
+</ivysettings>' >> /tmp/ivysettings.xml
+  java -jar /tmp/ivy-${IVY_VERSION}.jar -settings /tmp/ivysettings.xml -dependency org.apache.hive hive-jdbc 3.1.2 -retrieve "${JDBC_DRIVER_DIR}/[artifact]-[revision](-[classifier]).[ext]"
 }
 
 function install_stage_libs() {
