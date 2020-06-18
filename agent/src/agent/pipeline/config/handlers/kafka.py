23b61360ce20bf5f24cd8c054c87a0f578aec270
Anton Zelenin <ntnzelenin@gmail.com>
2020-06-18 13:52:34 +0300

    Set override_source in pipeline.set_config()

--- agent/src/agent/pipeline/config/handlers/kafka.py
+++ agent/src/agent/pipeline/config/handlers/kafka.py
@@ -20,11 +20,6 @@ class KafkaConfigHandler(BaseConfigHandler):
     def override_stages(self):
         # using 'anodot_agent_' + self.id as a default value in order not to break old configs
         if KafkaSource.CONFIG_CONSUMER_GROUP not in self.pipeline.override_source:
-            # todo this is a hotfix, please implement in properly
-            if KafkaSource.CONFIG_CONSUMER_GROUP in self.pipeline.config:
-                self.pipeline.override_source[KafkaSource.CONFIG_CONSUMER_GROUP] = \
-                    self.pipeline.config[KafkaSource.CONFIG_CONSUMER_GROUP]
-            else:
-                self.pipeline.override_source[KafkaSource.CONFIG_CONSUMER_GROUP] = 'anodot_agent_' + self.pipeline.id
+            self.pipeline.override_source[KafkaSource.CONFIG_CONSUMER_GROUP] = 'anodot_agent_' + self.pipeline.id
 
         super().override_stages()
