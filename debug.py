eccfbc76b26fc0c2c36706d9a48f8d0f269325c2
Daria Kharlan <dariakharlan@users.noreply.github.com>
2020-07-01 17:53:38 +0300

    Dev 1338 Influx fixes (#84)
    * don't put quotes around measurement name
    
    * add spaces to the query to support influx 1.1
    
    Co-authored-by: dariakharlan <daria@anodot.com>

--- debug.py
+++ debug.py
@@ -2,16 +2,8 @@ from agent.cli import source as source_cli
 from agent import cli
 
 # help('modules')
-#
+
 # cli.destination()
 # source_cli.create()
 # pipeline_cli.create()
 # cli.update()
-
-import requests
-sage_url = 'http://localhost:8888'
-user_token = '<JWT token>'
-body = {"clientId": "test-token-app", "groups": ["sage"], "permissions": ["search"]}
-
-resp = requests.post("%s/cc/api/tokens/" % sage_url, json=body, verify=False, headers={'Authorization': 'Bearer %s' % user_token})
-print(resp.content.decode('utf-8'))
