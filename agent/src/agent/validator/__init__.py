96c3fb7ebbc729386afd5f45d01452f2726f5120
Anton Zelenin <ntnzelenin@gmail.com>
2020-07-09 12:21:14 +0300

    dev-1312 code improvements

--- agent/src/agent/validator/__init__.py
+++ agent/src/agent/validator/__init__.py
@@ -2,8 +2,10 @@ from urllib.parse import urlparse
 
 from . import destination
 from . import proxy
+from ..tools import if_validation_enabled
 
 
+@if_validation_enabled
 def is_valid_url(url: str) -> bool:
     result = urlparse(url)
     return bool(result.netloc) and bool(result.scheme)
