6d13df96df355ce09f3d16e73583c7fb25a0fad1
Anton Zelenin <ntnzelenin@gmail.com>
2020-07-09 13:55:45 +0300

    test-1

--- agent/src/agent/logger.py
+++ agent/src/agent/logger.py
@@ -11,4 +11,5 @@ def get_logger(name, level=logging.DEBUG):
     file_handler = RotatingFileHandler(os.environ.get('LOG_FILE_PATH', 'agent.log'), maxBytes=2000)
     file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
     logger.addHandler(file_handler)
+    test = 1
     return logger
