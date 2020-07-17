edb8c82bac07495676cad85a944fff3f2717cceb
Anton Zelenin <ntnzelenin@gmail.com>
2020-07-17 12:47:39 +0300

    test anotmation fixes

--- agent/tests/test_antomation.py
+++ agent/tests/test_antomation.py
@@ -2,9 +2,12 @@ import os
 import subprocess
 
 from scripts.antomation.populate_sources_and_pipelines import ROOT_DIR
+from agent import cli
 
 
 def test_antomation():
     # if the script is not working the test will fail with an exception
     path = os.path.join(ROOT_DIR, 'populate_sources_and_pipelines.py')
     process = subprocess.Popen(['python', path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    cli.pipeline.delete('test_mongo_pipeline_antomation_1', 'test_mongo_pipeline_antomation_2')
+    cli.source.delete('test_mongo_antomation')
