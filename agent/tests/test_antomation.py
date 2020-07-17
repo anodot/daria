aa14b0499dda451c8e91c5b3aa4c58dfaf5bc472
Anton Zelenin <ntnzelenin@gmail.com>
2020-07-17 17:18:06 +0300

    Moved antomation scripts

--- agent/tests/test_antomation.py
+++ agent/tests/test_antomation.py
@@ -1,14 +1,13 @@
 import os
 import subprocess
 
-from scripts.antomation.populate_sources_and_pipelines import ROOT_DIR
 from agent import cli
 from .fixtures import cli_runner
 
 
 def test_antomation(cli_runner):
     # if the script is not working the test will fail with an exception
-    path = os.path.join(ROOT_DIR, 'populate_sources_and_pipelines.py')
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'scripts', 'antomation', 'populate_sources_and_pipelines.py')
     process = subprocess.Popen(['python', path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     cli_runner.invoke(cli.pipeline.delete, ['test_mongo_pipeline_antomation'])
     cli_runner.invoke(cli.source.delete, ['test_mongo_antomation'])
