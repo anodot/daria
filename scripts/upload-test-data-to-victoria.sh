94dba79a0c79c4ef9f54207b4aa7e41f53d2be28
Anton Zelenin <ntnzelenin@gmail.com>
2020-07-21 18:01:41 +0300

    Fix

--- scripts/upload-test-data-to-victoria.sh
+++ scripts/upload-test-data-to-victoria.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -X POST http://localhost:8428/api/v1/import -T test-datasets/test_victoria.jsonl
+curl -X POST http://victoriametrics:8428/api/v1/import -T test-datasets/test_victoria.jsonl
