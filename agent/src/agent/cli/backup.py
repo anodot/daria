bedd8aaeffc0a7f26776e3d853197996ccb10dcd
Anton Zelenin <ntnzelenin@gmail.com>
2020-09-21 18:54:27 +0300

    Using PGPASSWORD env var for pg_dump

--- agent/src/agent/cli/backup.py
+++ agent/src/agent/cli/backup.py
@@ -8,13 +8,11 @@ from agent.modules.constants import AGENT_DB_USER, POSTGRES_DB, BACKUP_DIRECTORY
 @click.command()
 def backup():
     filename = os.path.join(BACKUP_DIRECTORY, f'{POSTGRES_DB}_{datetime.now():%Y-%m-%d_%H:%M:%S}.bak')
-    os.system(f'pg_dump -h db -U {AGENT_DB_USER} {POSTGRES_DB} > {filename}')
-    print(f'{POSTGRES_DB} database successfully dumped to {filename}')
+    if os.system(f'pg_dump -h db -U {AGENT_DB_USER} {POSTGRES_DB} > {filename}') == 0:
+        print(f'{POSTGRES_DB} database successfully dumped to {filename}')
 
 
 @click.command()
 @click.argument('dump_file')
 def restore(dump_file):
     pass
-
-# pg_dump -h db -U agent agent > /backup-data/agent.bak
