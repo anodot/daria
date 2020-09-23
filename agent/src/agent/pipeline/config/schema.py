a80b28086acb717d8ac7c185a1447dd44b8a2206
Anton Zelenin <ntnzelenin@gmail.com>
2020-09-23 16:12:26 +0300

    Fix schema.update() function

--- agent/src/agent/pipeline/config/schema.py
+++ agent/src/agent/pipeline/config/schema.py
@@ -33,8 +33,7 @@ def equal(old_schema, new_schema) -> bool:
 
 def update(pipeline: p.Pipeline) -> dict:
     new_schema = build(pipeline)
-    api_client = anodot_api_client.AnodotApiClient(destination_.access_key, proxy.get_config(destination_.proxy), destination_.url)
-
+    api_client = anodot_api_client.get_client(pipeline.destination)
     old_schema = pipeline.get_schema()
     if old_schema:
         if equal(old_schema, new_schema):
