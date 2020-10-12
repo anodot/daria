eee863e8f78d82e5f5a599c189a312943b7dcc1d
Anton Zelenin <ntnzelenin@gmail.com>
2020-10-12 18:57:30 +0300

    Use uswgi instead of cron, added refresh_auth_token to endpoint decorator

--- agent/src/agent/destination/anodot_api_client.py
+++ agent/src/agent/destination/anodot_api_client.py
@@ -12,9 +12,10 @@ logger = get_logger(__name__)
 
 def endpoint(func):
     """
-    Decorator for api endpoints. Logs errors and returns json response
+    Logs errors and returns json response
     """
     def wrapper(*args, **kwargs):
+        args[0].refresh_session_authorization()
         res = func(*args, **kwargs)
         try:
             res.raise_for_status()
@@ -32,19 +33,18 @@ class AnodotApiClientException(click.ClickException):
 
 class AnodotApiClient:
     def __init__(self, destination_: HttpDestination):
-        self.destination_id = destination_.id
         self.url = destination_.url
         self.access_key = destination_.access_key
-        self.auth_token = destination_.auth_token
         self.proxies = proxy.get_config(destination_.proxy)
+        self.auth_token = destination_.auth_token if destination_.auth_token else self._create_token(destination_.id)
         self.session = requests.Session()
-        self._update_session_authorization()
+    
+    def _create_token(self, destination_id: int):
+        auth_token = AuthenticationToken(destination_id, self._retrieve_new_token())
+        destination.repository.save_auth_token(auth_token)
 
-    def _update_session_authorization(self):
-        if not self.auth_token:
-            self.auth_token = AuthenticationToken(self.destination_id, self._retrieve_new_token())
-            destination.repository.save_auth_token(self.auth_token)
-        elif self.auth_token.is_expired():
+    def refresh_session_authorization(self):
+        if self.auth_token.is_expired():
             self.auth_token.update(self._retrieve_new_token())
             destination.repository.save_auth_token(self.auth_token)
         self.session.headers.update({'Authorization': 'Bearer ' + self.auth_token.authentication_token})
@@ -60,21 +60,17 @@ class AnodotApiClient:
 
     @endpoint
     def create_schema(self, schema):
-        self._update_session_authorization()
         return self.session.post(self._build_url('stream-schemas'), json=schema, proxies=self.proxies)
 
     @endpoint
     def delete_schema(self, schema_id):
-        self._update_session_authorization()
         return self.session.delete(self._build_url('stream-schemas', schema_id), proxies=self.proxies)
 
     @endpoint
     def send_topology_data(self, data_type, data):
-        self._update_session_authorization()
         return self.session.post(self._build_url('topology', 'data'), proxies=self.proxies,
                                  data=data, params={'type': data_type})
 
     @endpoint
     def send_pipeline_data_to_bc(self, pipeline_data: dict):
-        self._update_session_authorization()
         return self.session.post(self._build_url('bc', 'agents'), proxies=self.proxies, json=pipeline_data)
