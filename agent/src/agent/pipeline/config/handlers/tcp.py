cdb4e6aad556ae534cf70a70a566394da7b7cfac
Дар'я Харлан <dadaka.x@gmail.com>
2020-10-28 14:48:11 +0200

    fix elastic tests

--- agent/src/agent/pipeline/config/handlers/tcp.py
+++ agent/src/agent/pipeline/config/handlers/tcp.py
@@ -9,7 +9,7 @@ class TCPConfigHandler(BaseConfigHandler):
     PIPELINE_BASE_CONFIG_NAME = 'tcp_server_http.json'
 
     stages_to_override = {
-        'source': stages.schemaless_source.Source,
+        'source': stages.source.Source,
         'JavaScriptEvaluator_01': stages.js_convert_metrics_20.JSConvertMetrics,
         'ExpressionEvaluator_02': stages.expression_evaluator.AddProperties,
         'ExpressionEvaluator_03': stages.expression_evaluator.Filtering,
