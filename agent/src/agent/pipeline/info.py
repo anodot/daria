4b584f5cb05f4e881b991780e7dbc564d70dc28c
Anton Zelenin <ntnzelenin@gmail.com>
2020-10-28 17:40:24 +0200

    Revert "Show none for pipeline info that is not present"
    This reverts commit c87fd53c

--- agent/src/agent/pipeline/info.py
+++ agent/src/agent/pipeline/info.py
@@ -74,8 +74,8 @@ def _extract_pipeline_issues(pipeline_info: dict) -> list:
 
 def _format(info: dict) -> str:
     keys = ['severity', 'configGroup', 'configName', 'message']
-    return ' - '.join(list(map(
-        lambda x: str(x),
+    return ' - '.join(list(filter(
+        lambda x: x is not None,
         map(info.get, keys)
     )))
 
