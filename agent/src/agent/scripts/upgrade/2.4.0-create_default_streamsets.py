f50d250a29d2b606451e1fd07d984f80e12a6f0c
Anton Zelenin <ntnzelenin@gmail.com>
2020-11-11 15:53:12 +0200

    Improvements for upgrade scripts

--- agent/src/agent/scripts/upgrade/2.4.0-create_default_streamsets.py
+++ agent/src/agent/scripts/upgrade/2.4.0-create_default_streamsets.py
@@ -1,9 +1,11 @@
 from agent import streamsets, pipeline
 from agent.modules import constants, db
-from agent.streamsets import StreamSets
+from agent.streamsets import StreamSets, get_logger
+
+logger = get_logger(__name__, stdout=True)
 
 if len(streamsets.repository.get_all()) > 0:
-    print('You already have a streamsets instance in the db')
+    logger.info('You already have a streamsets instance in the db')
     exit(1)
 
 streamsets.repository.save(StreamSets(
@@ -11,11 +13,13 @@ streamsets.repository.save(StreamSets(
     constants.DEFAULT_STREAMSETS_USERNAME,
     constants.DEFAULT_STREAMSETS_PASSWORD,
 ))
+logger.info(f'Created streamsets `{constants.DEFAULT_STREAMSETS_URL}`')
 
 streamsets_ = streamsets.repository.get_all()[0]
 for p in pipeline.repository.get_all():
     p.set_streamsets(streamsets_)
     pipeline.repository.save(p)
+    logger.info(f'Set streamsets for pipeline ID = {p.id}')
 
 # todo this is temporary
 db.session().commit()
