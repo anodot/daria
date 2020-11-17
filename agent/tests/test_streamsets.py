25de29bcf502369886ca1025140e0611387de12d
Anton Zelenin <ntnzelenin@gmail.com>
2020-11-17 17:09:16 +0200

    Don't allow to edit streamsets url

--- agent/tests/test_streamsets.py
+++ agent/tests/test_streamsets.py
@@ -1,21 +1,26 @@
 from agent import cli, streamsets
 
 url = 'http://dc:18630'
-input_ = {
+
+
+def test_streamsets_1(cli_runner):
+    input_ = {
         'url': url,
         'username': '',
         'password': '',
         'agent_external_url': '',
     }
-
-
-def test_streamsets_1(cli_runner):
     result = cli_runner.invoke(cli.streamsets.add, catch_exceptions=False, input='\n'.join(input_.values()))
     streamsets.repository.get_by_url(url)
     assert result.exit_code == 0
 
 
 def test_edit_streamsets(cli_runner):
+    input_ = {
+        'username': '',
+        'password': '',
+        'agent_external_url': '',
+    }
     result = cli_runner.invoke(cli.streamsets.edit, [url], catch_exceptions=False, input='\n'.join(input_.values()))
     streamsets.repository.get_by_url(url)
     assert result.exit_code == 0
