35c67086cfc2b6307c3d55fae299a98ab41498b2
Дар'я Харлан <dadaka.x@gmail.com>
2020-11-25 17:56:02 +0200

    save schema to the separate column because config is immutable

---
+++ agent/alembic/versions/87dc1408358b_add_schema_column_to_pipelines.py
@@ -0,0 +1,24 @@
+"""add_schema_column_to_pipelines
+
+Revision ID: 87dc1408358b
+Revises: 9d3d42cad294
+Create Date: 2020-11-25 15:34:32.641791
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '87dc1408358b'
+down_revision = '9d3d42cad294'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column('pipelines', sa.Column('schema', sa.JSON))
+
+
+def downgrade():
+    op.drop_column('streamsets', 'schema')
