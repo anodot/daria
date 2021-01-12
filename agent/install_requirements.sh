aa8d4388917e9fc276ee8b5b80e618f1dada2dc3
Anton Zelenin <ntnzelenin@gmail.com>
2021-01-12 13:43:17 +0200

    Fix requirements installation

--- agent/install_requirements.sh
+++ agent/install_requirements.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-pip install --upgrade pip && pip install -r requirements.txt
-
 # adding new repo to install correct postgres-client version
 wget --no-check-certificate --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 # using stretch instead of `lsb_release -cs`-pgdg main as it's not working with the current version
 echo "deb http://apt.postgresql.org/pub/repos/apt/ stretch-pgdg main" | tee  /etc/apt/sources.list.d/pgdg.list
 
-apt update && apt install -y vim netcat curl nano postgresql-client-12
+apt update && apt install -y --reinstall vim netcat curl nano ca-certificates postgresql-client-12
+
+pip install --upgrade pip && pip install -r requirements.txt
