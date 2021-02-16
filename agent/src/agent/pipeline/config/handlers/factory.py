5a9ac0ee59c28de6588d698213d151d1e033182a
Дар'я Харлан <dadaka.x@gmail.com>
2021-02-16 18:08:36 +0200

    fix

--- agent/src/agent/pipeline/config/handlers/factory.py
+++ agent/src/agent/pipeline/config/handlers/factory.py
@@ -26,7 +26,7 @@ def get_config_handler(pipeline_: Pipeline) -> BaseConfigHandler:
         source.TYPE_MYSQL: pipeline.config.handlers.jdbc.JDBCSchemaConfigHandler
     }
 
-    if pipeline_.uses_schema or pipeline_.source.type:
+    if pipeline_.uses_schema:
         return handlers_protocol30[pipeline_.source.type](pipeline_, base_config)
 
     return handlers_protocol20[pipeline_.source.type](pipeline_, base_config)
