b3663c297afbd33b9ee7816f893f21e804dd3276
Anton Zelenin <ntnzelenin@gmail.com>
2021-03-04 16:12:57 +0200

    Wip, bug fixes

--- agent/src/agent/api/routes/rrd_source.py
+++ agent/src/agent/api/routes/rrd_source.py
@@ -5,13 +5,16 @@ from agent.api.routes import needs_pipeline
 rrd_source = Blueprint('test', __name__)
 
 
-@rrd_source.route('/rrd_source/read/<pipeline_id>', methods=['POST'])
+@rrd_source.route('/rrd_source/fetch_data/<pipeline_id>', methods=['POST'])
 @needs_pipeline
 def read(pipeline_id: str):
+    pipeline_ = pipeline.repository.get_by_id(pipeline_id)
     metrics = source.rrd.extract_metrics(
-        pipeline.repository.get_by_id(pipeline_id).source,
+        pipeline_.source,
         str(request.get_json()['start']),
         str(request.get_json()['end']),
         str(request.get_json()['step']),
+        pipeline_.config['exclude_datasources'],
+        pipeline_.config['exclude_hosts'],
     )
     return jsonify(metrics)
--- agent/src/agent/cli/prompt/pipeline/cacti.py
+++ agent/src/agent/cli/prompt/pipeline/cacti.py
@@ -6,11 +6,21 @@ from agent.cli.prompt.pipeline import Prompter
 class CactiPrompter(Prompter):
     def prompt_config(self):
         self.prompt_interval('Polling interval in seconds')
-        # self.config['timestamp'] = {}
-        # self.config['timestamp']['type'] = 'unix'
+        self.prompt_exclude_hosts()
+        self.prompt_exclude_datasources()
         self.set_static_dimensions()
         self.set_tags()
 
-    def prompt_query(self):
-        self.config['query'] = click.prompt('Query to export data from VictoriaMetrics', type=click.STRING,
-                                            default=self.default_config.get('query'))
+    def prompt_exclude_hosts(self):
+        hosts = click.prompt(
+            'List of hosts to exclude separated by comma (example: host1, host2)', type=click.STRING,
+            default=','.join(self.default_config.get('exclude_hosts', []))
+        )
+        self.config['exclude_hosts'] = [host.strip() for host in hosts.split(',')]
+
+    def prompt_exclude_datasources(self):
+        ds = click.prompt(
+            'List of data sources to exclude separated by comma (example: ds1, ds2)', type=click.STRING,
+            default=','.join(self.default_config.get('exclude_datasources', []))
+        )
+        self.config['exclude_datasources'] = [host.strip() for host in ds.split(',')]
