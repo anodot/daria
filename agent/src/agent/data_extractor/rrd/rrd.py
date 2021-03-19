7d359ca1e83c774ccb1053a1f77a6553a5525e3a
Anton Zelenin <ntnzelenin@gmail.com>
2021-03-19 18:16:00 +0200

    Don't skip sources when there's no dimensions

--- agent/src/agent/data_extractor/rrd/rrd.py
+++ agent/src/agent/data_extractor/rrd/rrd.py
@@ -20,12 +20,9 @@ def extract_metrics(pipeline_: Pipeline, start: str, end: str, step: str) -> lis
         pipeline_.config.get('exclude_datasources')
     )
     for cacti_source in sources:
-        dimensions = _extract_dimensions(cacti_source)
-        if not dimensions:
-            continue
         base_metric = {
             'target_type': 'gauge',
-            'properties': dimensions,
+            'properties': _extract_dimensions(cacti_source),
         }
         rrd_file_name = cacti_source['data_source_path']
         if not rrd_file_name:
@@ -76,11 +73,9 @@ def _extract_dimensions(cacti_source: dict) -> dict:
     dimensions = {}
     dimension_values = _extract_dimension_values(source_name, cacti_source['name_cache'])
     if not dimension_values:
-        logger_.warning(f'Failed to extract dimension values from {source_name}, skipping')
         return {}
     dimension_names = _extract_dimension_names(source_name)
     if not dimension_names:
-        logger_.warning(f'Failed to extract dimension names from {source_name}, skipping')
         return {}
     for i, name in enumerate(dimension_names):
         dimensions[name] = dimension_values[i]
