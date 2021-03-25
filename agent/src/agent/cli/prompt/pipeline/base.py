7c8e02776235df91c1f37693f9c203efcf8be4c0
Anton Zelenin <ntnzelenin@gmail.com>
2021-03-25 13:50:06 +0200

    Fix cacti default dimensions to rename value

--- agent/src/agent/cli/prompt/pipeline/base.py
+++ agent/src/agent/cli/prompt/pipeline/base.py
@@ -169,7 +169,7 @@ class Prompter:
         self.config['uses_schema'] = pipeline.manager.supports_schema(self.pipeline) and static_what
 
     def prompt_rename_dimensions(self):
-        rename_dimensions_mapping = self.default_config.get('rename_dimensions_mapping', {})
+        rename_dimensions_mapping = self.get_default_object_value('rename_dimensions_mapping')
         if self.advanced:
             rename_dimensions_mapping = self.prompt_object(
                 'Dimensions to rename in format name1:alias1 name2:alias2',
