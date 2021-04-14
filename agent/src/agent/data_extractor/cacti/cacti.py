0f8e9acfcec4ebe0f666c0d41058ae9e016b7b3f
Anton Zelenin <ntnzelenin@gmail.com>
2021-04-14 16:01:01 +0300

    Always add dimensions to cacti metrics

--- agent/src/agent/data_extractor/cacti/cacti.py
+++ agent/src/agent/data_extractor/cacti/cacti.py
@@ -19,17 +19,26 @@ def extract_metrics(pipeline_: Pipeline, start: str, end: str, step: str) -> lis
     cacher = CactiCacher(pipeline_)
     variables = cacher.variables
     graphs = cacher.graphs
-    sources = cacher.sources
     hosts = cacher.hosts
 
     metrics = []
-    for local_graph_id, rrd_file_name in sources.items():
+    for local_graph_id, rrd_file_name in cacher.sources.items():
         if local_graph_id not in graphs:
             logger_.warning(f'local_graph_id `{local_graph_id}` is not in a list of graphs, skipping')
             continue
         if local_graph_id not in variables:
             logger_.warning(f'local_graph_id `{local_graph_id}` is not in a list of variables, skipping')
             continue
+        if not rrd_file_name:
+            continue
+        if '<path_rra>/' not in rrd_file_name:
+            logger_.warning(f'Path {rrd_file_name} does not contain "<path_rra>/", skipping')
+            continue
+        rrd_file_path = rrd_file_name.replace('<path_rra>', _get_rrd_dir(pipeline_))
+        if not os.path.isfile(rrd_file_path):
+            logger_.warning(f'File {rrd_file_path} does not exist')
+            continue
+
         base_metric = {
             'target_type': 'gauge',
             'properties': _extract_dimensions(
@@ -39,29 +48,18 @@ def extract_metrics(pipeline_: Pipeline, start: str, end: str, step: str) -> lis
                 pipeline_.config['add_graph_name_dimension']
             ),
         }
-        if not rrd_file_name:
-            continue
 
-        if '<path_rra>/' not in rrd_file_name:
-            logger_.warning(f'Path {rrd_file_name} does not contain "<path_rra>/", skipping')
-            continue
-
-        rrd_file_path = rrd_file_name.replace('<path_rra>', _get_rrd_dir(pipeline_))
-        if not os.path.isfile(rrd_file_path):
-            logger_.warning(f'File {rrd_file_path} does not exist')
-            continue
         result = rrdtool.fetch(rrd_file_path, 'AVERAGE', ['-s', start, '-e', end, '-r', step])
 
         # result[0][2] - is the closest available step to the step provided in the fetch command
-        # if they differ - skip the source as the desired step is not available in it
+        # if they differ - skip the source as the desired step is not available for it
         if result[0][2] != int(step):
             continue
 
-        # contains the timestamp of the first data item
-        data_start = result[0][0]
+        first_data_item_timestamp = result[0][0]
         for name_idx, measurement_name in enumerate(result[1]):
             for row_idx, data in enumerate(result[2]):
-                timestamp = int(data_start) + row_idx * int(step)
+                timestamp = int(first_data_item_timestamp) + row_idx * int(step)
                 value = data[name_idx]
 
                 # rrd might return a record for the timestamp earlier then start
@@ -85,36 +83,41 @@ def extract_metrics(pipeline_: Pipeline, start: str, end: str, step: str) -> lis
 def _extract_dimensions(graph_title: str, variables: dict, host: dict, add_graph_name_dimension=False) -> dict:
     dimensions = {}
     for var in _extract_dimension_names(graph_title):
-        if var.startswith('host_'):
-            value = _extract_host_field(var, host)
-            if value is None:
-                continue
-        elif var.startswith('query_'):
-            var_name = var.replace('query_', '')
-            if var_name not in variables:
-                logger_.warning(f'Variable `{var} is not know`')
-                continue
-            value = variables[var_name].strip()
-        else:
-            logger_.warning(f'Variable `{var} is not know`')
+        value = _extract(var, variables, host)
+        if value is None:
+            logger_.warning(f'Variable `{var}` does not exist`')
             continue
-        if not value:
-            logger_.warning(f'Value for {var} variable is empty, skipping')
+        if value == '':
+            logger_.warning(f'Value for `{var}` variable is empty, skipping')
             continue
-        dimensions[var] = value.replace(".", "_").replace(" ", "_")
+
+        dimensions[var] = value
     if add_graph_name_dimension:
         for k, v in dimensions.items():
             graph_title = graph_title.replace(f'|{k}|', v)
-        dimensions['graph_title'] = graph_title.replace(".", "_").replace(" ", "_")
-    return dimensions
+        dimensions['graph_title'] = graph_title
+    if 'host_description' not in dimensions:
+        dimensions['host_description'] = host['description']
+    return _replace_illegal_chars(dimensions)
 
 
-def _extract_host_field(variable: str, host: dict) -> Optional[str]:
-    var_name = variable.replace('host_', '')
-    if var_name not in host:
-        logger_.warning(f'Variable `{variable} is not know`')
+def _extract(variable: str, variables: dict, host: dict) -> Optional[str]:
+    if variable.startswith('host_'):
+        prefix = 'host_'
+        vars_ = host
+    elif variable.startswith('query_'):
+        prefix = 'query_'
+        vars_ = variables
+    else:
         return None
-    return host[var_name].strip()
+    var_name = variable.replace(prefix, '')
+    if var_name not in vars_:
+        return None
+    return vars_[var_name]
+
+
+def _replace_illegal_chars(dimensions: dict) -> dict:
+    return {re.sub('\s+', '_', k.strip().replace(".", "_")): v.strip().replace(".", "_").replace(" ", "_") for k, v in dimensions.items()}
 
 
 def _extract_rrd_archive(pipeline_: Pipeline):
