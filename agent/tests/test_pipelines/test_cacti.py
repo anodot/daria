4d8f985f2f84846a9d848cbb8333c854e0a477a6
Дар'я Харлан <dadaka.x@gmail.com>
2021-05-27 12:57:55 +0300

    return force stop

--- agent/tests/test_pipelines/test_cacti.py
+++ agent/tests/test_pipelines/test_cacti.py
@@ -9,11 +9,14 @@ class TestCacti(TestPipelineBase):
     __test__ = True
     params = {
         'test_start': [{'name': 'cacti_archive'}, {'name': 'cacti_dir'}, {'name': 'cacti_file', 'sleep': 30}],
+        'test_force_stop': [{'name': 'cacti_archive'}, {'name': 'cacti_dir'}, {'name': 'cacti_file'}],
         'test_output': [
             {'name': 'cacti_archive', 'output': 'cacti_archive.json', 'pipeline_type': source.TYPE_CACTI},
             {'name': 'cacti_dir', 'output': 'cacti_dir.json', 'pipeline_type': source.TYPE_CACTI},
             {'name': 'cacti_file', 'output': 'cacti_filtered.json', 'pipeline_type': source.TYPE_CACTI},
-        ]
+        ],
+        'test_delete_pipeline': [{'name': 'cacti_archive'}, {'name': 'cacti_dir'}, {'name': 'cacti_file'}],
+        'test_source_delete': [{'name': 'cacti_archive'}, {'name': 'cacti_dir'}, {'name': 'cacti_file'}],
     }
 
     def test_info(self, cli_runner, name=None):
@@ -31,14 +34,5 @@ class TestCacti(TestPipelineBase):
     def test_watermark(self):
         pytest.skip()
 
-    def test_force_stop(self, cli_runner, name=None):
-        pytest.skip()
-
     def test_output_schema(self, name=None, pipeline_type=None, output=None):
-        pytest.skip()
-
-    def test_delete_pipeline(self, cli_runner, name=None):
-        pytest.skip()
-
-    def test_source_delete(self, cli_runner, name=None):
         pytest.skip()
