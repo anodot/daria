4b4809b3afb43da199299ba1771198459cb5c0a7
Anton Zelenin <ntnzelenin@gmail.com>
2021-06-10 15:37:20 +0300

    Fix api tests

--- agent/src/agent/pipeline/pipeline.py
+++ agent/src/agent/pipeline/pipeline.py
@@ -108,8 +108,8 @@ class Pipeline(Entity, sdc_client.IPipeline):
     def set_config(self, config: dict):
         self._previous_config = deepcopy(self.config)
         self._previous_override_source = deepcopy(self.override_source)
+        self.override_source = config.pop(self.OVERRIDE_SOURCE, {})
         self.config = deepcopy(config)
-        self.override_source = deepcopy(self.config.get(self.OVERRIDE_SOURCE, {}))
 
     @property
     def source(self) -> Source:
