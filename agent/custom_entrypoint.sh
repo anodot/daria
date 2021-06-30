57cd8cbbd54e7cea031f5c73b81c577246971805
Anton Zelenin <ntnzelenin@gmail.com>
2021-06-30 16:45:22 +0300

    Added option to use openssl 1.0.2k in agent container (#266)
    * Added option to use openssl 1.0.2k in agent container

--- agent/custom_entrypoint.sh
+++ agent/custom_entrypoint.sh
@@ -5,4 +5,14 @@ set -u
 
 sleep 5 && alembic upgrade head
 
+OPEN_SSL_VERSION="${OPEN_SSL_VERSION:-1.1.1}"
+if [[ $OPEN_SSL_VERSION == '1.0.2k' ]]; then
+  mv /usr/bin/openssl /usr/bin/openssl.old
+  mv /usr/include/openssl /usr/include/openssl.old
+
+  ln -s /usr/local/openssl-1.0.2k/bin/openssl /usr/bin/openssl
+  ln -s /usr/local/openssl-1.0.2k/include/openssl/ /usr/include/openssl
+  echo /usr/local/openssl-1.0.2k/lib/ > /etc/ld.so.conf
+fi
+
 exec /entrypoint.sh "$@"
