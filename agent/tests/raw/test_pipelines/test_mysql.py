799bdb13702fe5294b616da17efc676d446797bb
Anton Zelenin <ntnzelenin@gmail.com>
2021-09-28 19:02:12 +0300

    Test more sleep for raw mysql test

--- agent/tests/raw/test_pipelines/test_mysql.py
+++ agent/tests/raw/test_pipelines/test_mysql.py
@@ -4,7 +4,7 @@ from .base import TestRawPipelineBase
 class TestRawMySQL(TestRawPipelineBase):
     __test__ = True
     params = {
-        'test_start': [{'name': 'mysql_raw'}],
+        'test_start': [{'name': 'mysql_raw', 'sleep': 20}],
         'test_force_stop': [{'name': 'mysql_raw'}],
         'test_output': [
             {'name': 'mysql_raw', 'output_file': 'mysql.json', 'pipeline_type': 'mysql'},
