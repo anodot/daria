1c1ff156c3502de4769b5c77dedefcfb4c980088
Anton Zelenin <ntnzelenin@gmail.com>
2021-11-16 14:23:35 +0200

    Fix

--- agent/src/agent/pipeline/config/jython_scripts/sleep.py
+++ agent/src/agent/pipeline/config/jython_scripts/sleep.py
@@ -9,6 +9,6 @@ finally:
 slept = False
 for record in sdc.records:
     if record.attributes['sdc.event.type'] == 'finished-file' and not slept:
-        time.sleep(sdc.userParams['SLEEP_TIME'])
+        time.sleep(int(sdc.userParams['SLEEP_TIME']))
         slept = True
     sdc.output.write(record)
