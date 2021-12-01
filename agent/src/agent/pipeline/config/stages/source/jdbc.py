fbaba3da7d3da8f1abd351daeeca987e111213bd
dariakharlan <daria@anodot.com>
2021-12-01 19:04:58 +0200

    [databricks] - fix get connection string function

--- agent/src/agent/pipeline/config/stages/source/jdbc.py
+++ agent/src/agent/pipeline/config/stages/source/jdbc.py
@@ -6,16 +6,16 @@ class JDBCSource(Stage):
     def get_config(self) -> dict:
         return {
             'query': pipeline.jdbc.query.Builder(self.pipeline).build(),
-            **{key: val for key, val in self.pipeline.source.config.items() if key.startswith('hikariConfigBean')},
             **self.get_connection_configs()
         }
 
     def get_connection_configs(self):
-        conf = {'hikariConfigBean.connectionString': 'jdbc:' + self.pipeline.source.config[
-            source.JDBCSource.CONFIG_CONNECTION_STRING]}
+        conn_str = 'jdbc:' + self.pipeline.source.config[source.JDBCSource.CONFIG_CONNECTION_STRING]
+        conf = {
+            'hikariConfigBean.connectionString': conn_str,
+            **{key: val for key, val in self.pipeline.source.config.items() if key.startswith('hikariConfigBean')}
+        }
         if self.pipeline.source.config.get(source.JDBCSource.CONFIG_USERNAME):
             conf['hikariConfigBean.useCredentials'] = True
-            conf['hikariConfigBean.username'] = self.pipeline.source.config[source.JDBCSource.CONFIG_USERNAME]
-            conf['hikariConfigBean.password'] = self.pipeline.source.config[source.JDBCSource.CONFIG_PASSWORD]
 
         return conf
