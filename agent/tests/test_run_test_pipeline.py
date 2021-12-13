5cbe84122e1d98e04536513de7f275bcc8a910d0
Vladyslav Poliukh <vpoliukh@anodot.com>
2021-12-13 17:27:07 +0200

    Update test

--- agent/tests/test_run_test_pipeline.py
+++ agent/tests/test_run_test_pipeline.py
@@ -6,6 +6,4 @@ def test_run_pipeline(cli_runner):
     print(f"run_test_pipeline: {result.exit_code} {result.output}")
 
     assert result.exit_code == 0
-    assert "Source run_test_pipeline created" in result.output
-    assert "Pipeline run_test_pipeline is starting" in result.output
-    assert "Test run OK" in result.output
+    assert "Test run completed" in result.output
