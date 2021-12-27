c1b909775a5e92a19236503e321f469d0aa4b9cd
Vladyslav Poliukh <vpoliukh@anodot.com>
2021-12-27 22:18:46 +0200

    Renamed mongosource with ecrypted pass

--- agent/tests/test_pipelines/test_mongo_http.py
+++ agent/tests/test_pipelines/test_mongo_http.py
@@ -8,24 +8,22 @@ class TestMongo(TestPipelineBase):
     params = {
         'test_start': [{'name': 'test_value_const'}, {'name': 'test_timestamp_ms'},
                        {'name': 'test_timestamp_string'}, {'name': 'test_timestamp_datetime'},
-                       {'name': 'test_mongo_file_short'}, {'name': 'test_mongo_file_full'},
-                       {'name': 'test_mongo_enc_file_short'}],
+                       {'name': 'test_mongo_file_short'},
+                       {'name': 'test_mongo_file_full'}],
         'test_reset': [{'name': 'test_value_const'}],
         'test_stop': [{'name': 'test_value_const'}, {'name': 'test_timestamp_ms'},
-                      {'name': 'test_timestamp_string'}, {'name': 'test_timestamp_datetime'},
-                      {'name': 'test_mongo_file_short'}, {'name': 'test_mongo_file_full'},
-                      {'name': 'test_mongo_enc_file_short'}],
+                       {'name': 'test_timestamp_string'}, {'name': 'test_timestamp_datetime'},
+                      {'name': 'test_mongo_file_short'},
+                       {'name': 'test_mongo_file_full'}],
         'test_output': [{'name': 'test_value_const', 'output': 'json_value_const_adv.json', 'pipeline_type': 'mongo'},
                         {'name': 'test_timestamp_ms', 'output': 'json_value_property.json', 'pipeline_type': 'mongo'},
-                        {'name': 'test_timestamp_string', 'output': 'json_value_property_adv.json',
-                         'pipeline_type': 'mongo'},
-                        {'name': 'test_timestamp_datetime', 'output': 'json_value_property.json',
-                         'pipeline_type': 'mongo'}],
+                        {'name': 'test_timestamp_string', 'output': 'json_value_property_adv.json', 'pipeline_type': 'mongo'},
+                        {'name': 'test_timestamp_datetime', 'output': 'json_value_property.json', 'pipeline_type': 'mongo'}],
         'test_delete_pipeline': [{'name': 'test_value_const'}, {'name': 'test_timestamp_ms'},
-                                 {'name': 'test_timestamp_string'}, {'name': 'test_timestamp_datetime'},
-                                 {'name': 'test_mongo_file_short'}, {'name': 'test_mongo_file_full'},
-                                 {'name': 'test_mongo_enc_file_short'}],
-        'test_source_delete': [{'name': 'test_mongo'}, {'name': 'test_mongo_1'}, {'name': 'test_mongo_enc'}]
+                       {'name': 'test_timestamp_string'}, {'name': 'test_timestamp_datetime'},
+                                 {'name': 'test_mongo_file_short'},
+                       {'name': 'test_mongo_file_full'}],
+        'test_source_delete': [{'name': 'test_mongo'}, {'name': 'test_mongo_1'}, {'name': 'test_mongo_encrypted_pass'}]
     }
 
     def test_info(self, cli_runner, name=None):
