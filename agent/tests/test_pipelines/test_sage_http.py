08f2faeb310ebe685cc267662084883acffb9aa4
Vladyslav Poliukh <vpoliukh@anodot.com>
2022-01-14 11:20:40 +0200

    Add start/stop/delete tests for sage_schema pipeline

--- agent/tests/test_pipelines/test_sage_http.py
+++ agent/tests/test_pipelines/test_sage_http.py
@@ -6,16 +6,34 @@ from .test_zpipeline_base import TestPipelineBase
 class TestSage(TestPipelineBase):
     __test__ = True
     params = {
-        'test_start': [{'name': 'test_sage_value_const'}, {'name': 'test_sage'}, {'name': 'test_sage_file'}],
-        'test_force_stop': [{'name': 'test_sage_value_const'}, {'name': 'test_sage'}, {'name': 'test_sage_file'}],
-        'test_reset': [{'name': 'test_sage_value_const'}],
-        'test_output': [{'name': 'test_sage_value_const', 'output': 'json_value_const_adv.json',
-                         'pipeline_type': 'sage'},
-                        {'name': 'test_sage', 'output': 'json_value_property.json',
-                         'pipeline_type': 'sage'}
-                        ],
-        'test_delete_pipeline': [{'name': 'test_sage_value_const'}, {'name': 'test_sage'}, {'name': 'test_sage_file'}],
-        'test_source_delete': [{'name': 'test_sage'}],
+        'test_start': [
+            {'name': 'test_sage_value_const'},
+            {'name': 'test_sage'},
+            {'name': 'test_sage_file'},
+            {'name': 'test_sage_schema_file'},
+        ],
+        'test_force_stop': [
+            {'name': 'test_sage_value_const'},
+            {'name': 'test_sage'},
+            {'name': 'test_sage_file'},
+            {'name': 'test_sage_schema_file'},
+        ],
+        'test_reset': [
+            {'name': 'test_sage_value_const'}
+        ],
+        'test_output': [
+            {'name': 'test_sage_value_const', 'output': 'json_value_const_adv.json', 'pipeline_type': 'sage'},
+            {'name': 'test_sage', 'output': 'json_value_property.json', 'pipeline_type': 'sage'}
+        ],
+        'test_delete_pipeline': [
+            {'name': 'test_sage_value_const'},
+            {'name': 'test_sage'},
+            {'name': 'test_sage_file'},
+            {'name': 'test_sage_schema_file'},
+        ],
+        'test_source_delete': [
+            {'name': 'test_sage'}
+        ],
     }
 
     def test_info(self, cli_runner, name=None):
