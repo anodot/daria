5099eb7fc425b1f4befb4559afbc4e93055bb93f
Anton Zelenin <ntnzelenin@gmail.com>
2022-02-28 16:07:37 +0200

    Include interval start and exclude interval end in jdbc queries (#359)
    * Include interval start and exclude interval end in jdbc queries
    
    * Fix test

--- agent/src/agent/pipeline/jdbc/query.py
+++ agent/src/agent/pipeline/jdbc/query.py
@@ -11,24 +11,24 @@ class Builder:
 
     def __init__(self, pipeline_: Pipeline):
         self.pipeline = pipeline_
-    
+
     def build(self):
         if isinstance(self.pipeline, pipeline.TestPipeline):
             return self._get_preview_query()
         query = self.pipeline.query.replace(f'{TIMESTAMP_CONDITION}', self._get_timestamp_condition())
         return query + ' ORDER BY ' + self.pipeline.timestamp_path
-    
+
     def _get_preview_query(self):
         if not self.pipeline.query:
             # dummy query for validating source connection in streamsets
             return 'SELECT * FROM t'
         query = self.pipeline.query.replace(f'{TIMESTAMP_CONDITION}', '1=1')
         return query + f' LIMIT {pipeline.manager.MAX_SAMPLE_RECORDS}'
-    
+
     def _get_timestamp_condition(self) -> str:
-        return f'{self._timestamp_to_unix()} > {self.TIMESTAMP_VALUE}' \
-               f' AND {self._timestamp_to_unix()} <= {self.TIMESTAMP_VALUE} + {self.pipeline.interval}'
-    
+        return f'{self._timestamp_to_unix()} >= {self.TIMESTAMP_VALUE}' \
+               f' AND {self._timestamp_to_unix()} < {self.TIMESTAMP_VALUE} + {self.pipeline.interval}'
+
     def _timestamp_to_unix(self):
         if self.pipeline.timestamp_type == pipeline.TimestampType.DATETIME:
             if self.pipeline.source.type == source.TYPE_POSTGRES:
@@ -44,10 +44,10 @@ class Builder:
                 return f"unix_timestamp({self.pipeline.timestamp_path})"
             if self.pipeline.source.type == source.TYPE_MSSQL:
                 return f"DATEDIFF(s, '1970-01-01 00:00:00', {self.pipeline.timestamp_path})"
-    
+
         if self.pipeline.timestamp_type == pipeline.TimestampType.UNIX_MS:
             return self.pipeline.timestamp_path + '/1000'
-    
+
         return self.pipeline.timestamp_path
 
 
