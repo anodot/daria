74a28e810b216cbaa90bac4020796220939ec7b1
Vladyslav Poliukh <vpoliukh@anodot.com>
2022-03-28 14:58:56 +0300

    [AL-10969] Add few more tests

--- agent/tests/unit/test_supports_schema.py
+++ agent/tests/unit/test_supports_schema.py
@@ -1,17 +1,39 @@
+import unittest
+from unittest.mock import Mock
 from agent import pipeline
 from agent import source
+from agent.pipeline import RawPipeline, TestPipeline
 
 
-class Object(object):
-    pass
+class TestPipelineSupportsSchema(unittest.TestCase):
+    def test_supports_schema(self):
+        for source_type in source.types:
+            source_ = Mock()
+            source_.type = source_type
 
+            pipeline_ = Mock()
+            pipeline_.source = source_
 
-def test_supports_schema():
-    for source_type in source.types:
-        source_ = Object()
-        source_.type = source_type
+            pipeline.manager.supports_schema(pipeline_)
 
-        pipeline_ = Object()
+    def test_supports_schema_exc(self):
+        with self.assertRaises(KeyError):
+            source_ = Mock()
+            source_.type = 'non_existing_type'
+            pipeline_ = Mock()
+            pipeline_.source = source_
+            pipeline.manager.supports_schema(pipeline_)
+
+    def test_supports_schema_TestPipeline(self):
+        source_ = Mock()
+        source_.type = 'some_type'
+        pipeline_ = Mock(spec=TestPipeline)
         pipeline_.source = source_
+        self.assertFalse(pipeline.manager.supports_schema(pipeline_))
 
-        pipeline.manager.supports_schema(pipeline_)
+    def test_supports_schema_RawPipeline(self):
+        source_ = Mock()
+        source_.type = 'some_type'
+        pipeline_ = Mock(spec=RawPipeline)
+        pipeline_.source = source_
+        self.assertFalse(pipeline.manager.supports_schema(pipeline_))
