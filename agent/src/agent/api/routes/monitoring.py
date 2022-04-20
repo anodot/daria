6f3a1a7c2d99575cca5efd77cfe50514ae12ee36
Anton Zelenin <ntnzelenin@gmail.com>
2022-04-20 16:50:35 +0300

    Fix

--- agent/src/agent/api/routes/monitoring.py
+++ agent/src/agent/api/routes/monitoring.py
@@ -1,25 +1,25 @@
 from flask import jsonify, Blueprint, request
-from agent import monitoring as monitoring_, destination, pipeline
+from agent import monitoring, destination, pipeline
 from prometheus_client import generate_latest, multiprocess
 
-multiprocess.MultiProcessCollector(monitoring_.metrics.registry)
+multiprocess.MultiProcessCollector(monitoring.metrics.registry)
 monitoring_bp = Blueprint('monitoring', __name__)
 
 
 @monitoring_bp.route('/metrics', methods=['GET'])
 def metrics():
-    monitoring_.pull_latest()
-    return generate_latest(registry=monitoring_.metrics.registry)
+    monitoring.pull_latest()
+    return generate_latest(registry=monitoring.metrics.registry)
 
 
 @monitoring_bp.route('/monitoring', methods=['GET'])
-def monitoring():
+def monitoring_():
     try:
         destination_ = destination.repository.get()
     except destination.repository.DestinationNotExists:
         return jsonify('')
 
-    if errors := monitoring_.data_sender.send_monitoring_data(destination_):
+    if errors := monitoring.sender.send_monitoring_data(destination_):
         raise MonitoringException(errors)
 
     return jsonify('')
@@ -28,26 +28,26 @@ def monitoring():
 @monitoring_bp.route('/monitoring/source_http_error/<pipeline_id>/<code>', methods=['POST'])
 def source_http_error(pipeline_id, code):
     pipeline_ = pipeline.repository.get_by_id(pipeline_id)
-    monitoring_.metrics.SOURCE_HTTP_ERRORS.labels(pipeline_id, pipeline_.source.type, code).inc(1)
+    monitoring.metrics.SOURCE_HTTP_ERRORS.labels(pipeline_id, pipeline_.source.type, code).inc(1)
     return jsonify('')
 
 
 @monitoring_bp.route('/monitoring/scheduled_script_error/<script_name>', methods=['POST'])
 def scheduled_script_error(script_name):
-    monitoring_.metrics.SCHEDULED_SCRIPTS_ERRORS.labels(script_name).inc(1)
+    monitoring.metrics.SCHEDULED_SCRIPTS_ERRORS.labels(script_name).inc(1)
     return jsonify('')
 
 
 @monitoring_bp.route('/monitoring/scheduled_script_execution_time/<script_name>', methods=['POST'])
 def scheduled_script_execution_time(script_name):
-    monitoring_.metrics.SCHEDULED_SCRIPT_EXECUTION_TIME.labels(script_name).set(request.json['duration'])
+    monitoring.metrics.SCHEDULED_SCRIPT_EXECUTION_TIME.labels(script_name).set(request.json['duration'])
     return jsonify('')
 
 
 @monitoring_bp.route('/monitoring/directory_file_processed/<pipeline_id>', methods=['POST'])
 def directory_file_processed(pipeline_id):
     pipeline_ = pipeline.repository.get_by_id(pipeline_id)
-    monitoring_.metrics.DIRECTORY_FILE_PROCESSED\
+    monitoring.metrics.DIRECTORY_FILE_PROCESSED\
         .labels(pipeline_.streamsets.url, pipeline_.name)\
         .inc(1)
     return jsonify('')
@@ -58,7 +58,7 @@ def watermark_delta(pipeline_id):
     delta = request.args.get('delta')
     assert delta or delta == 0
     pipeline_ = pipeline.repository.get_by_id(pipeline_id)
-    monitoring_.metrics.WATERMARK_DELTA\
+    monitoring.metrics.WATERMARK_DELTA\
         .labels(pipeline_.streamsets.url, pipeline_.name, pipeline_.source.type)\
         .set(delta)
     return jsonify('')
