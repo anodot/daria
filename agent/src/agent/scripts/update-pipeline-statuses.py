164c015cc43111b99ba775c9272c3a2be3962cef
Anton Zelenin <ntnzelenin@gmail.com>
2022-04-21 16:21:48 +0300

    Added option to use IGNORE_INIT_SCRIPT_FAILURES to ignore failures

--- agent/src/agent/scripts/update-pipeline-statuses.py
+++ agent/src/agent/scripts/update-pipeline-statuses.py
@@ -1,11 +1,14 @@
+import os
 import traceback
 import sdc_client
 
-from agent import pipeline, di, monitoring
+from agent import pipeline, di
 from agent.modules import logger
 
 di.init()
 logger = logger.get_logger(__name__)
+
+IGNORE_INIT_SCRIPT_FAILURES = os.environ.get('IGNORE_INIT_SCRIPT_FAILURES', 'false').lower() == 'true'
 
 
 def main():
@@ -15,9 +18,18 @@ def main():
             if pipeline_.name in statuses:
                 pipeline_.status = statuses[pipeline_.name]['status']
                 pipeline.repository.save(pipeline_)
-    except Exception:
-        monitoring.metrics.SCHEDULED_SCRIPTS_ERRORS.labels('update-pipeline-statuses.py').inc(1)
+    except Exception as e:
         logger.error(traceback.format_exc())
+
+        if not IGNORE_INIT_SCRIPT_FAILURES:
+            raise InitScriptException(
+                'Agent init script failed. You can set the '
+                '`IGNORE_INIT_SCRIPT_FAILURES` environment variable to `true` to start the container anyway'
+            ) from e
+
+
+class InitScriptException(Exception):
+    pass
 
 
 if __name__ == '__main__':
