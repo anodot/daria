178ef1bfc304c8bc2f4fcf472348f7e32d1f9bbd
Vladyslav Poliukh <vpoliukh@anodot.com>
2022-05-11 19:21:38 +0300

    [AL-11317] Update error message in validation

--- agent/src/agent/pipeline/validators/jdbc_query.py
+++ agent/src/agent/pipeline/validators/jdbc_query.py
@@ -3,10 +3,8 @@ from agent.pipeline.jdbc.query import TIMESTAMP_CONDITION, TIMESTAMP_COLUMN, LAS
 
 def get_errors(query: str):
     errors = []
-    if TIMESTAMP_CONDITION not in query:
-        errors.append(f'Please add {TIMESTAMP_CONDITION} constant to the query')
-        if any(i not in query for i in [TIMESTAMP_COLUMN, LAST_TIMESTAMP_VALUE, INTERVAL]):
-            errors.append(f'Please add {LAST_TIMESTAMP_VALUE}, {LAST_TIMESTAMP_VALUE}, {INTERVAL} constants to the query')
-        else:
-            errors.pop()
+    if TIMESTAMP_CONDITION not in query and \
+            any(i not in query for i in [TIMESTAMP_COLUMN, LAST_TIMESTAMP_VALUE, INTERVAL]):
+        errors.append(f'Please add either {TIMESTAMP_CONDITION} constant or '
+                      f'all of the following {LAST_TIMESTAMP_VALUE}, {LAST_TIMESTAMP_VALUE}, {INTERVAL} to the query')
     return errors
