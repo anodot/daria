46e80967b17597819870ebf257bf92d52af4e2d6
alexmuliar <omuliar@anodot.com>
2022-06-16 15:21:25 +0300

    AL-11286: Refactoring

--- agent/src/agent/modules/logger.py
+++ agent/src/agent/modules/logger.py
@@ -5,22 +5,13 @@ import sys
 from logging.handlers import RotatingFileHandler
 
 
-levels = {
-    'CRITICAL': logging.CRITICAL,
-    'FATAL': logging.FATAL,
-    'ERROR': logging.ERROR,
-    'WARN': logging.WARNING,
-    'WARNING': logging.WARNING,
-    'INFO': logging.INFO,
-    'DEBUG': logging.DEBUG,
-    'NOTSET': logging.NOTSET,
-}
-
-
 def get_logger(name, level=None, stdout=False):
     logger = logging.getLogger(name)
-    level = level or levels.get(os.environ.get('LOG_LEVEL'), logging.INFO)
-    logger.setLevel(level)
+    level = level or os.environ.get('LOG_LEVEL', logging.INFO)
+    try:
+        logger.setLevel(level)
+    except ValueError:
+        logger.setLevel(logging.INFO)
 
     file_handler = RotatingFileHandler(os.environ.get('LOG_FILE_PATH', 'agent.log'), maxBytes=2000)
     file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
