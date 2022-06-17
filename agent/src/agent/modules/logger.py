cdc04d2453cacc7fe19288ac27cf4ee504752ef9
alexmuliar <omuliar@anodot.com>
2022-06-17 15:28:26 +0300

    AL-11286: Allow agent fall if LOG_LEVEL is incorrect

--- agent/src/agent/modules/logger.py
+++ agent/src/agent/modules/logger.py
@@ -8,10 +8,7 @@ from logging.handlers import RotatingFileHandler
 def get_logger(name, level=None, stdout=False):
     logger = logging.getLogger(name)
     level = level or os.environ.get('LOG_LEVEL', logging.INFO)
-    try:
-        logger.setLevel(level)
-    except ValueError:
-        logger.setLevel(logging.INFO)
+    logger.setLevel(level)
 
     file_handler = RotatingFileHandler(os.environ.get('LOG_FILE_PATH', 'agent.log'), maxBytes=2000)
     file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
