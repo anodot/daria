0a4f3f36bdf546ace2ac16523f30285445bd3200
alexmuliar <omuliar@anodot.com>
2022-06-22 17:30:32 +0300

    AL-11556: Update tests

--- agent/tests/test_raw/test_pipelines/test_mysql.py
+++ agent/tests/test_raw/test_pipelines/test_mysql.py
@@ -7,7 +7,8 @@ class TestRawMySQL(TestRawPipelineBase):
         'test_start': [{'name': 'mysql_raw'}],
         'test_force_stop': [{'name': 'mysql_raw'}],
         'test_output': [
-            {'file_name': 'mysql_raw.json', 'output_file': 'mysql.json', 'pipeline_type': 'mysql'},
+            {'file_name': 'mysql_raw_json.json', 'output_file': 'mysql.json', 'pipeline_type': 'mysql'},
+            {'file_name': 'mysql_raw_csv.csv', 'output_file': 'mysql.csv', 'pipeline_type': 'mysql'},
         ],
         'test_delete_pipeline': [{'name': 'mysql_raw'}],
         'test_source_delete': [{'name': 'mysql_raw'}]
