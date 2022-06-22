092a2f5ba886eb34b6880c4ff950c959e09ff9e3
alexmuliar <omuliar@anodot.com>
2022-06-22 17:47:18 +0300

    AL-11556: update tests

--- agent/tests/test_raw/test_pipelines/test_mysql.py
+++ agent/tests/test_raw/test_pipelines/test_mysql.py
@@ -4,12 +4,24 @@ from .test_base import TestRawPipelineBase
 class TestRawMySQL(TestRawPipelineBase):
     __test__ = True
     params = {
-        'test_start': [{'name': 'mysql_raw'}],
-        'test_force_stop': [{'name': 'mysql_raw'}],
+        'test_start': [
+            {'name': 'mysql_raw_json'},
+            {'name': 'mysql_raw_csv'}
+        ],
+        'test_force_stop': [
+            {'name': 'mysql_raw_json'},
+            {'name': 'mysql_raw_csv'}
+        ],
         'test_output': [
             {'file_name': 'mysql_raw_json.json', 'output_file': 'mysql.json', 'pipeline_type': 'mysql'},
             {'file_name': 'mysql_raw_csv.csv', 'output_file': 'mysql.csv', 'pipeline_type': 'mysql'},
         ],
-        'test_delete_pipeline': [{'name': 'mysql_raw'}],
-        'test_source_delete': [{'name': 'mysql_raw'}]
+        'test_delete_pipeline': [
+            {'name': 'mysql_raw_json'},
+            {'name': 'mysql_raw_csv'}
+        ],
+        'test_source_delete': [
+            {'name': 'mysql_raw_json'},
+            {'name': 'mysql_raw_csv'}
+        ]
     }
