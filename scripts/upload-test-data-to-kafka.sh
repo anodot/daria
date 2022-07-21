27d662fc719dea96e7b44ba3005d253896fad39e
AlexMuliar <sashkomuliar@gmail.com>
2022-07-21 17:17:44 +0300

    AL-11745: fix tests

--- scripts/upload-test-data-to-kafka.sh
+++ scripts/upload-test-data-to-kafka.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-docker exec agent-kafka bash -c "kafka-console-producer.sh --broker-list localhost:9092 --topic test-partitions < /home/test.csv"
+docker exec agent-kafka bash -c "kafka-console-producer.sh --broker-list localhost:9092 --topic test-partitions --property parse.key=true < /home/test_partitions.csv"
 docker exec agent-kafka bash -c "kafka-console-producer.sh --broker-list localhost:9092 --topic test_csv < /home/test.csv"
 docker exec agent-kafka bash -c "kafka-console-producer.sh --broker-list localhost:9092 --topic test_kfk < /home/test_json_items_for_kafka"
 docker exec agent-kafka bash -c "kafka-console-producer.sh --broker-list localhost:9092 --compression-codec zstd --topic test_running_counters < /home/test_running_counter.txt"
 docker exec agent-kafka bash -c "kafka-console-producer.sh --broker-list localhost:9092 --compression-codec zstd --topic test_json_arrays < /home/test_with_arrays.json"
-sleep 30
-docker exec agent-kafka bash -c "kafka-console-producer.sh --broker-list localhost:9092 --topic test-partitions < /home/test.csv"
