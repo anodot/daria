4824846c02546a63766a60b0f8772deda5f2f139
vpoliukh <95220007+vpoliukh@users.noreply.github.com>
2022-07-22 17:30:51 +0200

    Fix set-thread-pool-size and set-heap-size commands (#428)

--- scripts/agent.sh
+++ scripts/agent.sh
@@ -25,13 +25,13 @@ elif [[ $1 == 'set-heap-size' ]]; then
     echo "Please use only integers to set the heap size"
     exit 1
   fi
-  sed -i '' -E 's/SDC_JAVA_OPTS.*/SDC_JAVA_OPTS: "-Xmx'$2'm -Xms'$2'm -server"/' docker-compose.yaml
+  sed -i -E 's/SDC_JAVA_OPTS.*/SDC_JAVA_OPTS: "-Xmx'$2'm -Xms'$2'm -server"/' docker-compose.yml
 elif [[ $1 == 'set-thread-pool-size' ]]; then
   if ! [[ $2 =~ ^[0-9]+$ ]]; then
     echo "Please use only integers to set the thread pool size"
     exit 1
   fi
-  sed -i '' -E 's/SDC_CONF_RUNNER_THREAD_POOL_SIZE.*/SDC_CONF_RUNNER_THREAD_POOL_SIZE: "'$2'"/' docker-compose.yaml
+  sed -i -E 's/SDC_CONF_RUNNER_THREAD_POOL_SIZE.*/SDC_CONF_RUNNER_THREAD_POOL_SIZE: "'$2'"/' docker-compose.yml
 elif [[ $1 == 'apply' ]]; then
   docker exec -i anodot-agent rm -rf /agent-data
   docker cp ./agent-data anodot-agent:/agent-data
