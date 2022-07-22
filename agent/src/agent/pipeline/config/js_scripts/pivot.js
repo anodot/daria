873962b2c09b50182d50ef6bf2d68f1224d10a98
AlexMuliar <sashkomuliar@gmail.com>
2022-07-22 15:26:53 +0300

    AL-11745: check for each record

--- agent/src/agent/pipeline/config/js_scripts/pivot.js
+++ agent/src/agent/pipeline/config/js_scripts/pivot.js
@@ -12,19 +12,20 @@ function extract_value(object, path) {
     }
     return object
 }
-if (!state['SELECTED_PARTITIONS'].size() || (records.length && state['SELECTED_PARTITIONS'][+records[0].attributes.partition])) {
-    for (var i = 0; i < records.length; i++) {
-        if (state['VALUES_ARRAY_PATH']) {
-            var items = extract_value(records[i].value, state['VALUES_ARRAY_PATH'])
+for (var i = 0; i < records.length; i++) {
+    if (state['SELECTED_PARTITIONS'].size() && state['SELECTED_PARTITIONS'][+records[i].attributes.partition] ) {
+        continue
+    }
+    if (state['VALUES_ARRAY_PATH']) {
+        var items = extract_value(records[i].value, state['VALUES_ARRAY_PATH'])
 
-            for (var l = 0; l < items.length; l++) {
-                var newRecord = sdcFunctions.createRecord(i + '_' + l);
-                newRecord.value = merge_two_objects(records[i].value, items[l])
-                output.write(newRecord)
-            }
-
-        } else {
-            output.write(records[i])
+        for (var l = 0; l < items.length; l++) {
+            var newRecord = sdcFunctions.createRecord(i + '_' + l);
+            newRecord.value = merge_two_objects(records[i].value, items[l])
+            output.write(newRecord)
         }
+
+    } else {
+        output.write(records[i])
     }
 }
