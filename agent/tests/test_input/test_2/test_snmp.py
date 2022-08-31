2f6112d7a12107994f5dde317eba2bfae6a55fbc
Vladyslav Poliukh <vpoliukh@anodot.com>
2022-08-31 17:02:46 +0300

    [AL-11843] Pipeline with filter

--- agent/tests/test_input/test_2/test_snmp.py
+++ agent/tests/test_input/test_2/test_snmp.py
@@ -4,6 +4,11 @@ from ..test_zpipeline_base import TestInputBase
 class TestSNMP(TestInputBase):
     __test__ = True
     params = {
-        'test_create_source_with_file': [{'file_name': 'snmp_sources'}],
-        'test_create_with_file': [{'file_name': 'snmp_pipelines'}],
+        'test_create_source_with_file': [
+            {'file_name': 'snmp/sources'}
+        ],
+        'test_create_with_file': [
+            {'file_name': 'snmp/pipelines'},
+            {'file_name': 'snmp/pipelines_filter'}
+        ],
     }
