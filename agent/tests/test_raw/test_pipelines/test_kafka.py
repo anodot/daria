8e968eada4c5b54f6fc30f895a0ce5357976d317
AlexMuliar <sashkomuliar@gmail.com>
2022-09-08 17:35:40 +0300

    AL-11997: fix config

--- agent/tests/test_raw/test_pipelines/test_kafka.py
+++ agent/tests/test_raw/test_pipelines/test_kafka.py
@@ -5,20 +5,20 @@ class TestRawKafka(TestRawPipelineBase):
     __test__ = True
     params = {
         'test_start': [
-            {'name': 'test_kafka_raw_csv'},
-            {'name': 'test_kafka_raw_json'},
+            {'name': 'kafka_raw_csv'},
+            {'name': 'kafka_raw_json'},
         ],
         'test_force_stop': [
-            {'name': 'test_kafka_raw_csv', 'check_output_file_name': 'test_kafka_raw_csv.csv'},
-            {'name': 'test_kafka_raw_json', 'check_output_file_name': 'test_kafka_raw_json.json'},
+            {'name': 'kafka_raw_csv', 'check_output_file_name': 'kafka_raw_csv.csv'},
+            {'name': 'kafka_raw_json', 'check_output_file_name': 'kafka_raw_json.json'},
         ],
         'test_output': [
-            {'file_name': 'test_kafka_raw_csv.csv', 'output_file': 'kafka.csv', 'pipeline_type': 'kafka'},
-            {'file_name': 'test_kafka_raw_json.json', 'output_file': 'kafka.json', 'pipeline_type': 'kafka'},
+            {'file_name': 'kafka_raw_csv.csv', 'output_file': 'kafka.csv', 'pipeline_type': 'kafka'},
+            {'file_name': 'kafka_raw_json.json', 'output_file': 'kafka.json', 'pipeline_type': 'kafka'},
         ],
         'test_delete_pipeline': [
-            {'name': 'test_kafka_raw_csv'},
-            {'name': 'test_kafka_raw_json'},
+            {'name': 'kafka_raw_csv'},
+            {'name': 'kafka_raw_json'},
         ],
         'test_source_delete': [
             {'name': 'test_kafka_raw_csv'},
