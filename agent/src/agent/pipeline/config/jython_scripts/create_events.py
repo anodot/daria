db331aee0c5d31a98d6fe028a432ef8667968de9
AlexMuliar <sashkomuliar@gmail.com>
2022-10-28 15:27:58 +0300

    AL-12144: validate each record inside script

--- agent/src/agent/pipeline/config/jython_scripts/create_events.py
+++ agent/src/agent/pipeline/config/jython_scripts/create_events.py
@@ -6,6 +6,18 @@ try:
 finally:
     sdc.importUnlock()
 
+
+def validate_event(event, record):
+    keys = [key for key in event]
+    if len(keys) == 0:
+        return False
+    for required_key in sdc.userParams["REQUIRED_FIELDS"]:
+        if required_key not in keys:
+            sdc.error.write(record, "Record Error: {} is required".format(sdc.userParams["REQUIRED_FIELDS"]))
+            return False
+    return True
+
+
 for rec in sdc.records:
     record = sdc.createRecord('record created ' + str(datetime.now()))
     event = {}
@@ -13,6 +25,8 @@ for rec in sdc.records:
         if v not in rec.value:
             continue
         event[k] = rec.value[v]
+    if not validate_event(event, record):
+        continue
     event['properties'] = []
     for prop in sdc.userParams['properties']:
         prop = {
