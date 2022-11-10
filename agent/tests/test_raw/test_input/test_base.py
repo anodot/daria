397be03750205e084796a55eb1dc2aed3bae36e5
Vladyslav Poliukh <vpoliukh@anodot.com>
2022-11-10 13:58:59 +0200

    [AL-12145] Fix raw test

--- agent/tests/test_raw/test_input/test_base.py
+++ agent/tests/test_raw/test_input/test_base.py
@@ -9,10 +9,9 @@ class TestRawInputBase(TestInputBase):
     __test__ = False
     params = {}
 
-    @pytest.mark.order(Order.SOURCE_CREATE)
+    @pytest.mark.order(Order.PIPELINE_CREATE)
     def test_create_raw_with_file(self, cli_runner, file_name, override_config: dict):
         self._test_create_with_file(cli_runner, file_name, override_config, cli.pipeline.create_raw)
 
-    @pytest.mark.order(Order.PIPELINE_CREATE)
     def test_create_with_file(self, cli_runner, file_name=None, override_config=None):
         pytest.skip()
