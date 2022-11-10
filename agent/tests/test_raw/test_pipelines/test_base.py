28334d6d362cb11777e8a3d0e0a7108984f42c36
Vladyslav Poliukh <vpoliukh@anodot.com>
2022-11-10 13:44:08 +0200

    [AL-12145] Base raw tests

--- agent/tests/test_raw/test_pipelines/test_base.py
+++ agent/tests/test_raw/test_pipelines/test_base.py
@@ -5,6 +5,7 @@ import pytest
 
 from agent.modules.constants import LOCAL_DESTINATION_OUTPUT_DIR
 from ...test_pipelines.test_zpipeline_base import TestPipelineBase
+from ...conftest import Order
 
 
 class TestRawPipelineBase(TestPipelineBase):
@@ -27,23 +28,28 @@ class TestRawPipelineBase(TestPipelineBase):
             self._wait(lambda: get_output(file_name))
             assert get_output(file_name)
 
+    @pytest.mark.order(Order.PIPELINE_START)
     def test_start(self, cli_runner, name: str, sleep: int):
         super(TestRawPipelineBase, self).test_start(cli_runner, name, sleep)
 
+    @pytest.mark.order(Order.PIPELINE_STOP)
     def test_force_stop(self, cli_runner, name, check_output_file_name):
         super(TestRawPipelineBase, self).test_force_stop(cli_runner, name, check_output_file_name)
         # give Streamsets time to send status change to agent
         time.sleep(1)
 
+    @pytest.mark.order(Order.PIPELINE_OUTPUT)
     def test_output(self, file_name, pipeline_type, output_file):
         expected_output = \
             read_file(os.path.join(os.path.dirname(os.path.realpath(__file__)), f'expected_output/{output_file}'))
         actual_output = get_output(file_name)
         assert expected_output == actual_output
 
+    @pytest.mark.order(Order.PIPELINE_DELETE)
     def test_delete_pipeline(self, cli_runner, name):
         super(TestRawPipelineBase, self).test_delete_pipeline(cli_runner, name)
 
+    @pytest.mark.order(Order.SOURCE_DELETE)
     def test_source_delete(self, cli_runner, name):
         super(TestRawPipelineBase, self).test_source_delete(cli_runner, name)
 
