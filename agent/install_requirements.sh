1e2d4f48764898d1760df43759ae1b497910968f
Дар'я Харлан <dadaka.x@gmail.com>
2022-12-09 17:18:01 +0400

    use correct postgres distribution

--- agent/install_requirements.sh
+++ agent/install_requirements.sh
@@ -5,7 +5,7 @@ set -e
 # adding new repo to install correct postgres-client version
 wget --no-check-certificate --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 # using stretch instead of `lsb_release -cs`-pgdg main as it's not working with the current version
-echo "deb http://apt.postgresql.org/pub/repos/apt/ buster-pgdg main" | tee /etc/apt/sources.list.d/pgdg.list
+echo "deb http://apt.postgresql.org/pub/repos/apt/ bullseye-pgdg main" | tee /etc/apt/sources.list.d/pgdg.list
 
 apt update && apt install -y --reinstall vim netcat curl nano ca-certificates postgresql-client-15 librrd-dev default-mysql-client kafkacat
 
