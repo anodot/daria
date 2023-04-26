d8725ee9ad5c9da1bb95de2f8f86cc16f9da7f2e
Anton Zelenin <ntnzelenin@gmail.com>
2023-04-26 22:24:29 +0300

    Influx to take into account tags in response (#490)
    * Influx to take into account tags in response

--- agent/tests/test_pipelines/test_1/test_influx_http.py
+++ agent/tests/test_pipelines/test_1/test_influx_http.py
@@ -46,24 +46,88 @@ class TestInflux(TestPipelineBase):
         ],
         'test_reset': [{'name': 'test_basic'}],
         'test_output': [
-            {'name': 'test_influx_file_short', 'output': 'influx.json', 'pipeline_type': 'influx'},
-            {'name': 'test_influx_file_full', 'output': 'influx_file_full.json', 'pipeline_type': 'influx'},
-            {'name': 'test_influx_adv', 'output': 'influx_adv.json', 'pipeline_type': 'influx'},
-            {'name': 'test_influx_file_transform', 'output': 'influx_file_transform.json', 'pipeline_type': 'influx'},
-            {'name': 'test_influx_tag', 'output': 'influx_tag.json', 'pipeline_type': 'influx'},
+            {
+                'name': 'test_influx_file_short',
+                'output': 'influx.json',
+                'pipeline_type': 'influx'
+            },
+            {
+                'name': 'test_influx_file_full',
+                'output': 'influx_file_full.json',
+                'pipeline_type': 'influx'
+            },
+            {
+                'name': 'test_influx_adv',
+                'output': 'influx_adv.json',
+                'pipeline_type': 'influx'
+            },
+            {
+                'name': 'test_influx_file_transform',
+                'output': 'influx_file_transform.json',
+                'pipeline_type': 'influx'
+            },
+            {
+                'name': 'test_influx_tag',
+                'output': 'influx_tag.json',
+                'pipeline_type': 'influx'
+            },
         ],
         'test_output_schema': [
-            {'name': 'test_basic', 'output': 'influx_schema.json', 'pipeline_type': 'influx'},
-            {'name': 'test_influx_schema_query', 'output': 'influx_schema_query.json', 'pipeline_type': 'influx'},
-            {'name': 'influx2_influxql_pipeline', 'output': 'influx2_influxql_schema.json', 'pipeline_type': 'influx'},
-            {'name': 'test_basic_offset', 'output': 'influx_offset_schema.json', 'pipeline_type': 'influx'},
-            {'name': 'test_influx2', 'output': 'influx2_schema.json', 'pipeline_type': 'influx2'},
-            {'name': 'test_influx2_file_full', 'output': 'influx2_file_schema.json', 'pipeline_type': 'influx2'},
-            {'name': 'test_influx2_query', 'output': 'influx2_query.json', 'pipeline_type': 'influx2'},
-            {'name': 'test_influx_schema_tag', 'output': 'influx_schema_tag.json', 'pipeline_type': 'influx'},
-            {'name': 'test_influx2_tag', 'output': 'influx2_tag.json', 'pipeline_type': 'influx2'},
-            {'name': 'test_influx2_query_join', 'output': 'influx2_query_join.json', 'pipeline_type': 'influx2'},
-            {'name': 'test_influx1_basic_auth', 'output': 'influx1_v2_basic_auth.json', 'pipeline_type': 'influx2'},
+            {
+                'name': 'test_basic',
+                'output': 'influx_schema.json',
+                'pipeline_type': 'influx'
+            },
+            {
+                'name': 'test_influx_schema_query',
+                'output': 'influx_schema_query.json',
+                'pipeline_type': 'influx'
+            },
+            {
+                'name': 'influx2_influxql_pipeline',
+                'output': 'influx2_influxql_schema.json',
+                'pipeline_type': 'influx'
+            },
+            {
+                'name': 'test_basic_offset',
+                'output': 'influx_offset_schema.json',
+                'pipeline_type': 'influx'
+            },
+            {
+                'name': 'test_influx2',
+                'output': 'influx2_schema.json',
+                'pipeline_type': 'influx2'
+            },
+            {
+                'name': 'test_influx2_file_full',
+                'output': 'influx2_file_schema.json',
+                'pipeline_type': 'influx2'
+            },
+            {
+                'name': 'test_influx2_query',
+                'output': 'influx2_query.json',
+                'pipeline_type': 'influx2'
+            },
+            {
+                'name': 'test_influx_schema_tag',
+                'output': 'influx_schema_tag.json',
+                'pipeline_type': 'influx'
+            },
+            {
+                'name': 'test_influx2_tag',
+                'output': 'influx2_tag.json',
+                'pipeline_type': 'influx2'
+            },
+            {
+                'name': 'test_influx2_query_join',
+                'output': 'influx2_query_join.json',
+                'pipeline_type': 'influx2'
+            },
+            {
+                'name': 'test_influx1_basic_auth',
+                'output': 'influx1_v2_basic_auth.json',
+                'pipeline_type': 'influx2'
+            },
         ],
         'test_delete_pipeline': [
             {'name': 'test_basic'},
@@ -116,6 +180,7 @@ class TestInflux(TestPipelineBase):
             expected_output = get_expected_output(name, output, pipeline_type)
             actual_output = get_output(f'{name}_{pipeline_type}.json')
             return actual_output == expected_output
+
         self._wait(compare_output)
         assert compare_output()
 
@@ -128,7 +193,13 @@ class TestInflux(TestPipelineBase):
         initial_offset = 1552222380
         interval = 1200000
         schema_id = get_schema_id('test_basic')
-        assert get_output(f'{schema_id}_watermark.json') == {'watermark': initial_offset + interval, 'schemaId': schema_id}
+        assert get_output(f'{schema_id}_watermark.json') == {
+            'watermark': initial_offset + interval,
+            'schemaId': schema_id
+        }
         initial_offset = 1552999980
         schema_id = get_schema_id('test_basic_offset')
-        assert get_output(f'{schema_id}_watermark.json') == {'watermark': initial_offset + interval, 'schemaId': schema_id}
+        assert get_output(f'{schema_id}_watermark.json') == {
+            'watermark': initial_offset + interval,
+            'schemaId': schema_id
+        }
