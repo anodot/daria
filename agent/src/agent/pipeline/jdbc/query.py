abe7b89b80b44d03286e8d7f6e89c453db8b2306
dariakharlan <dadaka.x@gmail.com>
2024-01-09 12:11:01 +0400

    druid: don't put quotes arount timestamp if not __time

--- agent/src/agent/pipeline/jdbc/query.py
+++ agent/src/agent/pipeline/jdbc/query.py
@@ -53,7 +53,11 @@ class Builder:
 
     def _get_indexed_query(self) -> str:
         if self.pipeline.source.type == source.TYPE_DRUID:
-            return f'TIME_IN_INTERVAL("{self.pipeline.timestamp_path}", ' \
+            timestamp = self.pipeline.timestamp_path
+            if self.pipeline.timestamp_path != '__time':
+                # if timestamp is not default then we assume an aggregation function is used and we don't need quotes
+                timestamp = f"{self.pipeline.timestamp_path}"
+            return f'TIME_IN_INTERVAL({timestamp}, ' \
                    f'\'{self.TIMESTAMP_VALUE_ISO}/PT{self.pipeline.interval}S\')'
         if self.pipeline.source.type == source.TYPE_MSSQL:
             return f"{self.pipeline.timestamp_path} BETWEEN DATEADD(second, {self.TIMESTAMP_VALUE}, '1970-01-01') AND "\
