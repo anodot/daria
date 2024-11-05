4aa2b078e73a46c6831dabd6e83e9ad904ab0c25
Vladyslav Poliukh <vpoliukh@anodot.com>
2024-11-05 16:44:48 +0200

    AL-11656 Test pipeline create

--- agent/tests/test_input/test_2/test_dynatrace_http.py
+++ agent/tests/test_input/test_2/test_dynatrace_http.py
@@ -9,8 +9,15 @@ class TestDynatrace(TestInputBase):
         'test_create_source_with_file': [
             {'file_name': 'dynatrace/sources'},
         ],
+        'test_create_with_file': [{
+            'file_name': 'dynatrace/pipelines'
+        }],
     }
 
     @pytest.mark.order(Order.SOURCE_CREATE)
     def test_create_source_with_file(self, cli_runner, file_name):
         super().test_create_source_with_file(cli_runner, file_name)
+
+    @pytest.mark.order(Order.PIPELINE_CREATE)
+    def test_create_with_file(self, cli_runner, file_name, override_config):
+        super().test_create_with_file(cli_runner, file_name, override_config)
